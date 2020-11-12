88e9284909a8461e560bfe24fd72911dbc71d496
David-B-Comer <davec2233@gmail.com>
2020-11-12 18:36:39 -0500

    added question 15

--- OracleStudyGuide/Assessment/Solutions.java
+++ OracleStudyGuide/Assessment/Solutions.java
@@ -367,6 +367,29 @@ What is the result of the following class? (Choose all that apply)
 
 
  Answer D, E, F
+
+
+
+ Question 15
+
+ Which of the following statements can be inserted in the blank line so that the code will
+ compile successfully? (Choose all that apply)
+ public interface CanSwim {}
+ public class Amphibian implements CanSwim {}
+ class Tadpole extends Amphibian {}
+ public class FindAllTadPole {
+ public static void main(String[] args) {
+ List<Tadpole> tadpoles = new ArrayList<Tadpole>();
+ for(Amphibian amphibian : tadpoles) {
+ ___________ tadpole = amphibian;
+ } } }
+
+
+ A. CanSwim
+ B. Long
+ C. Amphibian
+ D. Tadpole
+ E. Object
  **/
 
 public class Solutions {
