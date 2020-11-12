67c5f6f7ecc4d0ffb79264d816c1d2630adc8ebd
David-B-Comer <davec2233@gmail.com>
2020-11-12 17:02:45 -0500

    added question 6

--- OracleStudyGuide/Assessment/Solutions.java
+++ OracleStudyGuide/Assessment/Solutions.java
@@ -130,6 +130,29 @@ What is the result of the following class? (Choose all that apply)
  F. An uncaught exception is thrown
 
  Answer D
+
+
+
+ Question 6
+
+ What is the result of the following program?
+ 1: public class MathFunctions {
+ 2: public static void addToInt(int x, int amountToAdd) {
+ 3: x = x + amountToAdd;
+ 4: }
+ 5: public static void main(String[] args) {
+ 6: int a = 15;
+ 7: int b = 10;
+ 8: MathFunctions.addToInt(a, b);
+ 9: System.out.println(a); } }
+
+
+ A. 10
+ B. 15
+ C. 25
+ D. Compiler error on line 3.
+ E. Compiler error on line 8.
+ F. None of the above.
  **/
 
 public class Solutions {
