03b0795bc2b6a7031e38780a7e9b333e7f5ca09d
David-B-Comer <davec2233@gmail.com>
2020-11-12 16:56:50 -0500

    added question 5

--- OracleStudyGuide/Assessment/Solutions.java
+++ OracleStudyGuide/Assessment/Solutions.java
@@ -105,6 +105,30 @@ What is the result of the following class? (Choose all that apply)
 
  Answer C
 
+
+
+ Question 5
+
+ What is the output of the following code snippet?
+ 13: System.out.print("a");
+ 14: try {
+ 15: System.out.print("b");
+ 16: throw new IllegalArgumentException();
+ 17: } catch (RuntimeException e) {
+ 18: System.out.print("c");
+ 19: } finally {
+ 20: System.out.print("d");
+ 21: }
+ 22: System.out.print("e");
+
+
+ A. abe
+ B. abce
+ C. abde
+ D. abcde
+ E. The code does not compile.
+ F. An uncaught exception is thrown
+
  **/
 
 public class Solutions {
