043f425936b6290c6e9271452252095f79aa2f6f
David-B-Comer <davec2233@gmail.com>
2020-11-12 16:23:14 -0500

    added question 3

--- OracleStudyGuide/Assessment/Solutions.java
+++ OracleStudyGuide/Assessment/Solutions.java
@@ -45,6 +45,35 @@ What is the result of the following class? (Choose all that apply)
 
  Answer C
 
+
+
+ Question 3
+
+ What is the output of the following code? (Choose all that apply)
+ 1: interface HasTail { int getTailLength(); }
+ 2: abstract class Puma implements HasTail {
+ 3: protected int getTailLength() {return 4;}
+ 4: }
+ 5: public class Cougar extends Puma {
+ xxxii Introduction
+ fl ast.indd 1½ 4/2014 Page xxxii
+ 6: public static void main(String[] args) {
+ 7: Puma puma = new Puma();
+ 8: System.out.println(puma.getTailLength());
+ 9: }
+ 10:
+ 11: public int getTailLength(int length) {return 2;}
+ 12: }
+
+
+ A. 2
+ B. 4
+ C. The code will not compile because of line 3.
+ D. The code will not compile because of line 5.
+ E. The code will not compile because of line 7.
+ F. The code will not compile because of line 11.
+ G. The output cannot be determined from the code provided.
+
  **/
 
 public class Solutions {
