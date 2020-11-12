dac6d21da5da5b45aff6cf61cfa22b5289f38c9c
David-B-Comer <davec2233@gmail.com>
2020-11-11 22:17:11 -0500

    added question 2

--- OracleStudyGuide/Assessment/Solutions.java
+++ OracleStudyGuide/Assessment/Solutions.java
@@ -1,6 +1,8 @@
 package Assessment;
 
-/** Question 1
+/**
+
+Question 1
 
 What is the result of the following class? (Choose all that apply)
         1: public class _C {
@@ -20,6 +22,27 @@ What is the result of the following class? (Choose all that apply)
         G. nullnull
 
  Answer E
+
+
+
+ Question 2
+
+ What is the result of the following code?
+ String s1 = "Java";
+ String s2 = "Java";
+ StringBuilder sb1 = new StringBuilder();
+ sb1.append("Ja").append("va");
+ System.out.println(s1 == s2);
+ System.out.println(s1.equals(s2));
+ System.out.println(sb1.toString() == s1);
+ System.out.println(sb1.toString().equals(s1));
+
+ A. true is printed out exactly once.
+ B. true is printed out exactly twice.
+ C. true is printed out exactly three times.
+ D. true is printed out exactly four times.
+ E. The code does not compile.
+
  **/
 
 public class Solutions {
