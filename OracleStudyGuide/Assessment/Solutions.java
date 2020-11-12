f4761ccbd70f7c34c1f8c6adda1c0db7b010bfac
David-B-Comer <davec2233@gmail.com>
2020-11-12 17:10:36 -0500

    added question 7

--- OracleStudyGuide/Assessment/Solutions.java
+++ OracleStudyGuide/Assessment/Solutions.java
@@ -156,6 +156,29 @@ What is the result of the following class? (Choose all that apply)
 
 
  Answer B
+
+
+
+ Question 7
+
+ What is the result of the following code?
+ int[] array = {6,9,8};
+ List<Integer> list = new ArrayList<>();
+ xxxiv Introduction
+ fl ast.indd 1½ 4/2014 Page xxxiv
+ list.add(array[0]);
+ list.add(array[2]);
+ list.set(1, array[1]);
+ list.remove(0);
+ System.out.println(list);
+
+
+ A. [8]
+ B. [9]
+ C. Something like [Ljava.lang.String;@160bc7c0
+ D. An exception is thrown.
+ E. The code does not compile.
+
  **/
 
 public class Solutions {
