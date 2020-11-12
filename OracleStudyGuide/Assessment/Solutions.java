8f7564a1a698682310870c5ce33975bdf8aaa237
David-B-Comer <davec2233@gmail.com>
2020-11-12 18:17:58 -0500

    added question 13

--- OracleStudyGuide/Assessment/Solutions.java
+++ OracleStudyGuide/Assessment/Solutions.java
@@ -316,6 +316,26 @@ What is the result of the following class? (Choose all that apply)
 
  Answer B
 
+
+
+ Question 13
+
+ What is the output of the following program?
+ 1: public class BearOrShark {
+ 2: public static void main(String[] args) {
+ 3: int luck = 10;
+ 4: if((luck>10 ? luck++: --luck)<10) {
+ 5: System.out.print("Bear");
+ 6: } if(luck<10) System.out.print("Shark");
+ 7: } }
+
+
+ A. Bear
+ B. Shark
+ C. BearShark
+ D. The code will not compile because of line 4.
+ E. The code will not compile because of line 6.
+ F. The code compiles without issue but does not produce any output.
  **/
 
 public class Solutions {
