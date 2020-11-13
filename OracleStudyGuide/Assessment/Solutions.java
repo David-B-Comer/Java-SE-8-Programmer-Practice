4be1f3e599d809ba45039dc795dc713f7474ecc4
David-B-Comer <davec2233@gmail.com>
2020-11-13 09:11:20 -0500

    added question 18

--- OracleStudyGuide/Assessment/Solutions.java
+++ OracleStudyGuide/Assessment/Solutions.java
@@ -438,6 +438,19 @@ What is the result of the following class? (Choose all that apply)
 
  Answer A, C, F
 
+
+
+
+ Question 18
+
+ Which of the following print out a date representing April 1, 2015? (Choose all that apply)
+ A. System.out.println(LocalDate.of(2015, Calendar.APRIL, 1));
+ B. System.out.println(LocalDate.of(2015, Month.APRIL, 1));
+ C. System.out.println(LocalDate.of(2015, 3, 1));
+ D. System.out.println(LocalDate.of(2015, 4, 1));
+ E. System.out.println(new LocalDate(2015, 3, 1));
+ F. System.out.println(new LocalDate(2015, 4, 1));
+
  **/
 
 public class Solutions {
