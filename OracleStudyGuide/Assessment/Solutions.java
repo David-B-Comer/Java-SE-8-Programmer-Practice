fb5ad4863364c45650d509714739e00882d4ceaf
David-B-Comer <davec2233@gmail.com>
2020-11-13 08:58:11 -0500

    added question 17

--- OracleStudyGuide/Assessment/Solutions.java
+++ OracleStudyGuide/Assessment/Solutions.java
@@ -413,6 +413,29 @@ What is the result of the following class? (Choose all that apply)
  G. Override the getName() method with a concrete method in the Otter class.
 
  Answer D, F, G
+
+
+
+ Question 17
+
+ Which of the following lines can be inserted at line 11 to print true? (Choose all that
+ apply)
+ 10: public static void main(String[] args) {
+ 11: // INSERT CODE HERE
+ 12: }
+ 13: private static boolean test(Predicate<Integer> p) {
+ 14: return p.test(5);
+ 15: }
+
+
+ A. System.out.println(test(i -> i == 5));
+ B. System.out.println(test(i -> {i == 5;}));
+ C. System.out.println(test((i) -> i == 5));
+ D. System.out.println(test((int i) -> i == 5);
+ E. System.out.println(test((int i) -> {return i == 5;}));
+ F. System.out.println(test((i) -> {return i == 5;}));
+
+ 
  **/
 
 public class Solutions {
