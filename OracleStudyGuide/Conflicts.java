53c934ad51b8dbeea405a9a65a9160e32fb7e870
David-B-Comer <davec2233@gmail.com>
2020-11-14 16:59:10 -0500

    added conflicts class import alternative

--- OracleStudyGuide/Conflicts.java
+++ OracleStudyGuide/Conflicts.java
@@ -1,6 +1,12 @@
-import java.util.Date;
+/**import java.util.Date;
 
 public class Conflicts {
     Date date;
+    java.sql.Date sqlDate;
+}
+**/
+
+public class Conflicts {
+    java.util.Date date;
     java.sql.Date sqlDate;
 }
