904795c6344506226510faed5584731abdcd317d
David-B-Comer <davec2233@gmail.com>
2020-11-15 14:23:45 -0500

    added java primitive types chart

--- OracleStudyGuide/JavaPrimitiveTypes.java
+++ OracleStudyGuide/JavaPrimitiveTypes.java
@@ -1,2 +1,13 @@
 public class JavaPrimitiveTypes {
+/**
+    Keyword     Type                            Example
+    boolean     true or false                   true
+    byte        8-bit integral value            123
+    short       16-bit integral value           123
+    int         32-bit integral value           123
+    long        64-bit integral value           123
+    float       32-bit floating-point value     123.45f
+    double      64-bit floating-point value     123.456
+    char        16-bit Unicode value            'a'
+ **/
 }
