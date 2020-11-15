8cb83b4326366bd4a3127b238eaf3bc7834b07ee
David-B-Comer <davec2233@gmail.com>
2020-11-15 13:44:29 -0500

    added chick class constructor

--- OracleStudyGuide/Chick.java
+++ OracleStudyGuide/Chick.java
@@ -1,2 +1,9 @@
 public class Chick {
+    private String name = "Fluffy";
+    { System.out.println("setting field"); }
+
+    public Chick() {
+        name = "Tiny";
+        System.out.println("setting constructor");
+        }
 }
