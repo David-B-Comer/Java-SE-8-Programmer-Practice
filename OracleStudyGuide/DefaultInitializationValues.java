ed3ca6d67b40c1f3fff1ca7084115478442b717a
David-B-Comer <davec2233@gmail.com>
2020-11-15 16:04:37 -0500

    added default initialization values list

--- OracleStudyGuide/DefaultInitializationValues.java
+++ OracleStudyGuide/DefaultInitializationValues.java
@@ -1,2 +1,12 @@
 public class DefaultInitializationValues {
+/**
+    Variable type                               Default initialization value
+    boolean                                     false
+    byte, short, int, long                      0 (in the type’s bit-length)
+    float, double                               0.0 (in the type’s bit-length)
+    char                                        '\u0000' (NUL)
+    All object references (everything else)     null
+
+ **/
 }
+
