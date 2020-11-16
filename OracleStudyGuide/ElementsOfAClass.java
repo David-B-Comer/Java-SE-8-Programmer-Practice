58adcfba4c98cfeb0420aec0385bb69e54b82685
David-B-Comer <davec2233@gmail.com>
2020-11-16 17:48:19 -0500

    added elements of a class table

--- OracleStudyGuide/ElementsOfAClass.java
+++ OracleStudyGuide/ElementsOfAClass.java
@@ -1,2 +1,12 @@
 public class ElementsOfAClass {
+    /**
+
+     Element                    Example                 Required?           Where does it go?
+     Package declaration        package abc;            No                  First line in the file
+     Import statements          import java.util.*;     No                  Immediately after the package
+     Class declaration          public class C          Yes                 Immediately after the import
+     Field declarations         int value;              No                  Anywhere inside a class
+     Method declarations        void method()           No                  Anywhere inside a class
+
+     **/
 }
