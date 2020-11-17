343d9e7adf65f3d704f4490085eac4e79611f858
David-B-Comer <davec2233@gmail.com>
2020-11-16 20:35:43 -0500

    added chapter 1 review question 4

--- OracleStudyGuide/Chapter1/Chapter1ReviewQuestions.java
+++ OracleStudyGuide/Chapter1/Chapter1ReviewQuestions.java
@@ -59,5 +59,27 @@ public class Chapter1ReviewQuestions {
 
 
      Answer B, D, E
+
+
+
+     4. Given the following class, which of the following is true? (Choose all that apply)
+
+     1: public class Snake {
+     2:
+     3: public void shed(boolean time) {
+     4:
+     5: if (time) {
+     6:
+     7: }
+     8: System.out.println(result);
+     9:
+     10: }
+     11: }
+
+     A. If String result = "done"; is inserted on line 2, the code will compile.
+     B. If String result = "done"; is inserted on line 4, the code will compile.
+     C. If String result = "done"; is inserted on line 6, the code will compile.
+     D. If String result = "done"; is inserted on line 9, the code will compile.
+     E. None of the above changes will make the code compile.
      **/
 }
