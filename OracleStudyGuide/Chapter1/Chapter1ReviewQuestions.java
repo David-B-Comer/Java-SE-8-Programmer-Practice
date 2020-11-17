3947e48fce269e6c862d9bf6951de9454b098e50
David-B-Comer <davec2233@gmail.com>
2020-11-16 21:08:19 -0500

    added chapter 1 review question 10

--- OracleStudyGuide/Chapter1/Chapter1ReviewQuestions.java
+++ OracleStudyGuide/Chapter1/Chapter1ReviewQuestions.java
@@ -205,5 +205,20 @@ public class Chapter1ReviewQuestions {
 
 
     Answer A, C, D, E
+
+
+
+     10. Which of the following are legal entry point methods that can be run from the command
+     line? (Choose all that apply)
+
+     A. private static void main(String[] args)
+     B. public static final main(String[] args)
+     C. public void main(String[] args)
+     D. public static void test(String[] args)
+     E. public static void main(String[] args)
+     F. public static main(String[] args)
+     G. None of the above.
+
+
      **/
 }
