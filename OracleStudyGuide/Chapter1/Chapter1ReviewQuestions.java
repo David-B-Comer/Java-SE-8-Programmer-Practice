087675814d6a1d1909bb257895806b2a283b49b2
David-B-Comer <davec2233@gmail.com>
2020-11-16 20:27:09 -0500

    added chapter 1 review question 3

--- OracleStudyGuide/Chapter1/Chapter1ReviewQuestions.java
+++ OracleStudyGuide/Chapter1/Chapter1ReviewQuestions.java
@@ -37,5 +37,24 @@ public class Chapter1ReviewQuestions {
      F. Empty = null, Brand = null
 
      Answer D
+
+
+
+     3. Which of the following are true? (Choose all that apply)
+
+     4: short numPets = 5;
+     5: int numGrains = 5.6;
+     6: String name = "Scruffy";
+     7: numPets.length();
+     8: numGrains.length();
+     9: name.length();
+
+     A. Line 4 generates a compiler error.
+     B. Line 5 generates a compiler error.
+     C. Line 6 generates a compiler error.
+     D. Line 7 generates a compiler error.
+     E. Line 8 generates a compiler error.
+     F. Line 9 generates a compiler error.
+     G. The code compiles as is.
      **/
 }
