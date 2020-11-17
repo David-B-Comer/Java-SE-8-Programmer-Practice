08caa62bb389f92f2097cd1976cf72ebaf74dd2f
David-B-Comer <davec2233@gmail.com>
2020-11-16 19:21:50 -0500

    added chapter 1 review question 2

--- OracleStudyGuide/Chapter1/Chapter1ReviewQuestions.java
+++ OracleStudyGuide/Chapter1/Chapter1ReviewQuestions.java
@@ -3,17 +3,39 @@ package Chapter1;
 public class Chapter1ReviewQuestions {
 
     /**
-     *
+
     1. Which of the following are valid Java identifiers? (Choose all that apply)
 
      A. A$B
-    B. _helloWorld
-    C. true
-    D. java.lang
-    E. Public
-    F. 1980_s
+     B. _helloWorld
+     C. true
+     D. java.lang
+     E. Public
+     F. 1980_s
 
 
      Answer A, B, E
+
+
+
+     2. What is the output of the following program?
+
+     1: public class WaterBottle {
+     2: private String brand;
+     3: private boolean empty;
+     4: public static void main(String[] args) {
+     5: WaterBottle wb = new WaterBottle();
+     6: System.out.print("Empty = " + wb.empty);
+     7: System.out.print(", Brand = " + wb.brand);
+     8: } }
+
+     A. Line 6 generates a compiler error.
+     B. Line 7 generates a compiler error.
+     C. There is no output.
+     D. Empty = false, Brand = null
+     E. Empty = false, Brand =
+     F. Empty = null, Brand = null
+
+
      **/
 }
