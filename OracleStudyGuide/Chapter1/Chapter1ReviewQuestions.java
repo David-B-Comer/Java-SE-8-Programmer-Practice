d1aa6c3692798da886fb2f1ce679e19b000633b9
David-B-Comer <davec2233@gmail.com>
2020-11-16 19:17:05 -0500

    added chapter 1 review question 1

--- OracleStudyGuide/Chapter1/Chapter1ReviewQuestions.java
+++ OracleStudyGuide/Chapter1/Chapter1ReviewQuestions.java
@@ -1,4 +1,17 @@
 package Chapter1;
 
 public class Chapter1ReviewQuestions {
+
+    /**
+     *
+    1. Which of the following are valid Java identifiers? (Choose all that apply)
+
+     A. A$B
+    B. _helloWorld
+    C. true
+    D. java.lang
+    E. Public
+    F. 1980_s
+
+     **/
 }
