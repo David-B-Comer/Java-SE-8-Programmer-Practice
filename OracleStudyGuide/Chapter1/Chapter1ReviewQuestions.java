6cb0aca7176d09d6b7e2e0f106fb94fd97ffed27
David-B-Comer <davec2233@gmail.com>
2020-11-16 20:38:07 -0500

    added chapter 1 review question 5

--- OracleStudyGuide/Chapter1/Chapter1ReviewQuestions.java
+++ OracleStudyGuide/Chapter1/Chapter1ReviewQuestions.java
@@ -84,5 +84,28 @@ public class Chapter1ReviewQuestions {
 
 
      Answer A, B
+
+
+
+     5. Given the following classes, which of the following can independently replace INSERT
+     IMPORTS HERE to make the code compile? (Choose all that apply)
+
+     package aquarium;
+     public class Tank { }
+     package aquarium.jellies;
+     public class Jelly { }
+     package visitor;
+     INSERT IMPORTS HERE
+     public class AquariumVisitor {
+     public void admire(Jelly jelly) { } }
+
+     A. import aquarium.*;
+     B. import aquarium.*.Jelly;
+     C. import aquarium.jellies.Jelly;
+     D. import aquarium.jellies.*;
+     E. import aquarium.jellies.Jelly.*;
+     F. None of these can make the code compile.
+
+
      **/
 }
