3f2ed1038d2011ded2040bbf5a57fe6d75c69978
David-B-Comer <davec2233@gmail.com>
2020-11-17 21:00:24 -0500

    added chapter 1 review question 17

--- OracleStudyGuide/Chapter1/Chapter1ReviewQuestions.java
+++ OracleStudyGuide/Chapter1/Chapter1ReviewQuestions.java
@@ -322,5 +322,22 @@ public class Chapter1ReviewQuestions {
 
      Answer B, C, D
 
+
+
+     17. Which of the following are true? (Choose all that apply)
+
+     public class Bunny {
+     public static void main(String[] args) {
+     Bunny bun = new Bunny();
+     } }
+
+     A. Bunny is a class.
+     B. bun is a class.
+     C. main is a class.
+     D. Bunny is a reference to an object.
+     E. bun is a reference to an object.
+     F. main is a reference to an object.
+     G. None of the above.
+
      **/
 }
