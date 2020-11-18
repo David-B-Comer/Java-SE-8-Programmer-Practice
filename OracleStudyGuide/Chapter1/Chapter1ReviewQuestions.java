9363ba7752203fb3f2aed96f4aaaf3d491fc275e
David-B-Comer <davec2233@gmail.com>
2020-11-17 21:03:52 -0500

    added chapter 1 review question 18

--- OracleStudyGuide/Chapter1/Chapter1ReviewQuestions.java
+++ OracleStudyGuide/Chapter1/Chapter1ReviewQuestions.java
@@ -342,5 +342,21 @@ public class Chapter1ReviewQuestions {
 
      Answer A, E
 
+
+
+     18. Which represent the order in which the following statements can be assembled into a program that will compile successfully? (Choose all that apply)
+
+     A: class Rabbit {}
+     B: import java.util.*;
+     C: package animals;
+
+     A. A, B, C
+     B. B, C, A
+     C. C, B, A
+     D. B, A
+     E. C, A
+     F. A, C
+     G. A, B
+
      **/
 }
