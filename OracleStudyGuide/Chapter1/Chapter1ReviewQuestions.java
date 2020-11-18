71b0cd8ea0797970bd10f2200164d056e9d82a9a
David-B-Comer <davec2233@gmail.com>
2020-11-17 20:48:44 -0500

    added chapter 1 review question 13

--- OracleStudyGuide/Chapter1/Chapter1ReviewQuestions.java
+++ OracleStudyGuide/Chapter1/Chapter1ReviewQuestions.java
@@ -251,5 +251,17 @@ public class Chapter1ReviewQuestions {
 
      Answer G
 
+
+
+     13. Which of the following are true? (Choose all that apply)
+
+     A. An instance variable of type boolean defaults to false.
+     B. An instance variable of type boolean defaults to true.
+     C. An instance variable of type boolean defaults to null.
+     D. An instance variable of type int defaults to 0.
+     E. An instance variable of type int defaults to 0.0.
+     F. An instance variable of type int defaults to null.
+     G. None of the above.
+
      **/
 }
