7c580af9763bd26e6d7d127ed227a8a24058b0e7
David-B-Comer <davec2233@gmail.com>
2020-11-17 20:53:58 -0500

    added chapter 1 review question 15

--- OracleStudyGuide/Chapter1/Chapter1ReviewQuestions.java
+++ OracleStudyGuide/Chapter1/Chapter1ReviewQuestions.java
@@ -285,5 +285,17 @@ public class Chapter1ReviewQuestions {
 
 
      Answer D
+
+
+
+     15. Which of the following lines of code compile? (Choose all that apply)
+
+     A. int i1 = 1_234;
+     B. double d1 = 1_234_.0;
+     C. double d2 = 1_234._0;
+     D. double d3 = 1_234.0_;
+     E. double d4 = 1_234.0;
+     F. None of the above
+
      **/
 }
