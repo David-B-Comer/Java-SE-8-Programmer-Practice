ef05ab1caf984eae25aaefee3e23165189fbd611
David-B-Comer <davec2233@gmail.com>
2020-11-17 21:15:52 -0500

    added chapter 1 review question 20

--- OracleStudyGuide/Chapter1/Chapter1ReviewQuestions.java
+++ OracleStudyGuide/Chapter1/Chapter1ReviewQuestions.java
@@ -395,5 +395,26 @@ public class Chapter1ReviewQuestions {
 
      Answer B, D
 
+
+
+     20. What is true about the following code? (Choose all that apply)
+
+     public class Bear {
+     protected void finalize() {
+     System.out.println("Roar!");
+     }
+     public static void main(String[] args) {
+     Bear bear = new Bear();
+     bear = null;
+     System.gc();
+     } }
+
+     A. finalize() is guaranteed to be called.
+     B. finalize() might or might not be called
+     C. finalize() is guaranteed not to be called.
+     D. Garbage collection is guaranteed to run.
+     E. Garbage collection might or might not run.
+     F. Garbage collection is guaranteed not to run.
+     G. The code does not compile.
      **/
 }
