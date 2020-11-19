92e2e112670a8e7c41f10c60acaed88e8bd55fcc
David-B-Comer <davec2233@gmail.com>
2020-11-18 23:11:25 -0500

    added order of operator precedence table

--- OracleStudyGuide/Chapter2/OrderOfOperatorPrecedence.java
+++ OracleStudyGuide/Chapter2/OrderOfOperatorPrecedence.java
@@ -1,4 +1,22 @@
 package Chapter2;
 
 public class OrderOfOperatorPrecedence {
+
+    /**
+
+     Operator                               Symbols and examples
+     Post-unary operators                   expression++, expression--
+     Pre-unary operators                    ++expression, --expression
+     Other unary operators                  +, -, !
+     Multiplication/Division/Modulus        *, /, %
+     Addition/Subtraction                   +, -
+     Shift operators                        <<, >>, >>>
+     Relational operators                   <, >, <=, >=, instanceof
+     Equal to/not equal to                  ==, !=
+     Logical operators                      &, ^, |
+     Short-circuit logical operators        &&, ||
+     Ternary operators                      boolean expression ? expression1 : expression2
+     Assignment operators                   =, +=, -=, *=, /=, %=, &=, ^=,  !=, <<=, >>=, >>>=
+
+     **/
 }
