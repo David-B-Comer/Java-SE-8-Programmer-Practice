8c2658616ff7cc69efe3f8e9bff291dfb1907c2f
David-B-Comer <davec2233@gmail.com>
2020-11-19 22:14:56 -0500

    added relational operators table

--- OracleStudyGuide/Chapter2/RelationalOperators.java
+++ OracleStudyGuide/Chapter2/RelationalOperators.java
@@ -1,4 +1,13 @@
 package Chapter2;
 
 public class RelationalOperators {
+
+    /**
+
+     <      Strictly less than
+     <=     Less than or equal to
+     >      Strictly greater than
+     >=     Greater than or equal to
+
+     **/
 }
