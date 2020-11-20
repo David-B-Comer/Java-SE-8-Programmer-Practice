9e80b2ef0884509a77990e21ee3e2a58e11954c6
David-B-Comer <davec2233@gmail.com>
2020-11-19 21:39:06 -0500

    added unary operators table

--- OracleStudyGuide/Chapter2/UnaryOperators.java
+++ OracleStudyGuide/Chapter2/UnaryOperators.java
@@ -1,4 +1,14 @@
 package Chapter2;
 
 public class UnaryOperators {
+    /**
+
+    Unary operator                 Description
+    +                              Indicates a number is positive, although numbers are assumed to be positive in Java unless accompanied by a negative unary operator
+    -                              Indicates a literal number is negative or negates an expression
+    ++                             Increments a value by 1
+    --                             Decrements a value by 1
+    !                              Inverts a Boolean’s logical value
+
+     **/
 }
