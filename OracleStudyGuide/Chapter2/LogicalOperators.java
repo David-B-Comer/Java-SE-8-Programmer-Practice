4983feec2f50d004c8605c4529c68d5c75572c79
David-B-Comer <davec2233@gmail.com>
2020-11-20 18:57:26 -0500

    added logical operator & explanation

--- OracleStudyGuide/Chapter2/LogicalOperators.java
+++ OracleStudyGuide/Chapter2/LogicalOperators.java
@@ -1,4 +1,27 @@
 package Chapter2;
 
 public class LogicalOperators {
+
+    /**
+
+     x & y
+     (AND)
+
+     x = true
+     y = true
+     result = true
+
+     x = true
+     y = false
+     result = false
+
+     x = false
+     y = true
+     result = false
+
+     x = false
+     y = false
+     result = false
+
+     **/
 }
