56a2491e3612a2aee38309263c0ae245f4589500
David-B-Comer <davec2233@gmail.com>
2020-11-21 19:34:17 -0500

    added if then statement explanation

--- OracleStudyGuide/Chapter2/IfThenStatement.java
+++ OracleStudyGuide/Chapter2/IfThenStatement.java
@@ -1,4 +1,10 @@
 package Chapter2;
 
 public class IfThenStatement {
+
+    /**
+
+     The if-then statement executes executes a particular block of code if and only if a boolean expression evaluates to true at runtime.
+
+     **/
 }
