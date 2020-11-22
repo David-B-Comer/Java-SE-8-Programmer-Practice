08667ceda121a2507d68acbef6e339f52a4e73d6
David-B-Comer <davec2233@gmail.com>
2020-11-21 19:01:47 -0500

    added equality operators scenarios

--- OracleStudyGuide/Chapter2/EqualityOperators.java
+++ OracleStudyGuide/Chapter2/EqualityOperators.java
@@ -1,4 +1,15 @@
 package Chapter2;
 
 public class EqualityOperators {
+
+    /**
+
+     The equality operators are used in one of three scenarios:
+     1. Comparing two numeric primitive types. If the numeric values are of different data
+     types, the values are automatically promoted as previously described. For example,
+     5 == 5.00 returns true since the left side is promoted to a double.
+     2. Comparing two boolean values.
+     3. Comparing two objects, including null and String values.
+
+     **/
 }
