fd1dcb6001e54e3513c5f722edd914c9c5f736e3
David-B-Comer <davec2233@gmail.com>
2020-11-22 19:57:06 -0500

    added ternary operator explanation

--- OracleStudyGuide/Chapter2/TernaryOperator.java
+++ OracleStudyGuide/Chapter2/TernaryOperator.java
@@ -1,4 +1,13 @@
 package Chapter2;
 
 public class TernaryOperator {
+
+    /**
+
+     booleanExpression ? expression1 : expression2
+
+     The first operand must be a boolean expression, and the second and third can be any
+     expression that returns a value. The ternary operation is really a condensed form of an ifthen-else statement that returns a value.
+
+     **/
 }
