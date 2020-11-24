13811c2d41b3a352aaa7dad916c3fcfc22594599
David-B-Comer <davec2233@gmail.com>
2020-11-23 21:45:32 -0500

    added while loop explanation

--- OracleStudyGuide/Chapter2/WhileLoop.java
+++ OracleStudyGuide/Chapter2/WhileLoop.java
@@ -1,4 +1,11 @@
 package Chapter2;
 
 public class WhileLoop {
+    /**
+
+     A repetition control structure, which we refer to as a loop, executes a statement of code
+     multiple times in succession. By using nonconstant variables, each repetition of the statement may be different. For example, a statement that iterates over a list of unique names
+     and outputs them would encounter a new name on every execution of the loop.
+
+     **/
 }
