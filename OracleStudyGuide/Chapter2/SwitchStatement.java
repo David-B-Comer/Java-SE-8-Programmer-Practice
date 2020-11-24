c3bb23785c2f27fd4da8e7ee1795880dd3d1f990
David-B-Comer <davec2233@gmail.com>
2020-11-23 21:24:25 -0500

    added switch statement data types

--- OracleStudyGuide/Chapter2/SwitchStatement.java
+++ OracleStudyGuide/Chapter2/SwitchStatement.java
@@ -7,5 +7,15 @@ public class SwitchStatement {
      matching branch, known as a case statement. If no such case statement is found that matches the value, an optional
      default statement will be called. If no such default option is available, the entire switch statement will be skipped.
 
+
+     Data types supported by switch statements include the following:
+     ■ int and Integer
+     ■ byte and Byte
+     ■ short and Short
+     ■ char and Character
+     ■ int and Integer
+     ■ String
+     ■ enum values
+
      **/
 }
