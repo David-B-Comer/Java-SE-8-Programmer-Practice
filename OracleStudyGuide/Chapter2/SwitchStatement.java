a2031ac9d9497df818e71cf2fc452caf0c47cfe9
David-B-Comer <davec2233@gmail.com>
2020-11-23 20:55:21 -0500

    added switch statement explanation

--- OracleStudyGuide/Chapter2/SwitchStatement.java
+++ OracleStudyGuide/Chapter2/SwitchStatement.java
@@ -1,4 +1,11 @@
 package Chapter2;
 
 public class SwitchStatement {
+    /**
+
+     A complex decision-making structure in which a single value is evaluated and fl ow is redirected to the first
+     matching branch, known as a case statement. If no such case statement is found that matches the value, an optional
+     default statement will be called. If no such default option is available, the entire switch statement will be skipped.
+
+     **/
 }
