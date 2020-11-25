eb932896195583b86b0ef3d13dbc2507ece468ea
David-B-Comer <davec2233@gmail.com>
2020-11-24 21:22:18 -0500

    added for statement additional explanation

--- OracleStudyGuide/Chapter2/ForStatement.java
+++ OracleStudyGuide/Chapter2/ForStatement.java
@@ -7,5 +7,10 @@ public class ForStatement {
      statements, as the other loops you have seen, as well as two new sections: an initialization
      block and an update statement.
 
+     Variables declared in the initialization block of a for loop have limited scope and
+     are only accessible within the for loop. Be wary of any exam questions in which a variable declared within the initialization block of a for loop is available outside the loop.
+     Alternatively, variables declared before the for loop and assigned a value in the initialization block may be used outside the for loop because their scope precedes the for loop
+     creation.
+
      **/
 }
