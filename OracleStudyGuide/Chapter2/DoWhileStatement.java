28c0a42abf1d6e0a117e565a6a7dff691bd0cd27
David-B-Comer <davec2233@gmail.com>
2020-11-24 21:16:16 -0500

    added do while statement additional explanation

--- OracleStudyGuide/Chapter2/DoWhileStatement.java
+++ OracleStudyGuide/Chapter2/DoWhileStatement.java
@@ -6,5 +6,10 @@ public class DoWhileStatement {
      A repetition control structure with a termination condition and statement, or block of statements.
      Unlike a while loop, a do-while loop guarantees that the statement or block will be executed at least once.
 
+     The primary difference between the syntactic structure of a do-while loop and a while
+     loop is that a do-while loop purposely orders the statement or block of statements before
+     the conditional expression, in order to reinforce that the statement will be executed before
+     the expression is ever evaluated.
+
      **/
 }
