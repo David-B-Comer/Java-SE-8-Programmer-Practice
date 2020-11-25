144c8ea788b7221c3fc18c3194ef5762d6147a3f
David-B-Comer <davec2233@gmail.com>
2020-11-24 21:20:14 -0500

    added for statement explanation

--- OracleStudyGuide/Chapter2/ForStatement.java
+++ OracleStudyGuide/Chapter2/ForStatement.java
@@ -1,4 +1,11 @@
 package Chapter2;
 
 public class ForStatement {
+    /**
+
+     A basic for loop has the same conditional boolean expression and statement, or block of
+     statements, as the other loops you have seen, as well as two new sections: an initialization
+     block and an update statement.
+
+     **/
 }
