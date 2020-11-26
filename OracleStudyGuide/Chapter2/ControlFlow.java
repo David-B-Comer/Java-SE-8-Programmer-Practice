46719db8780f28edb3449ab0f48bf59e538f0919
David-B-Comer <davec2233@gmail.com>
2020-11-25 22:15:33 -0500

    added control flow table

--- OracleStudyGuide/Chapter2/ControlFlow.java
+++ OracleStudyGuide/Chapter2/ControlFlow.java
@@ -1,4 +1,14 @@
 package Chapter2;
 
 public class ControlFlow {
+    /**
+
+                Allows optional labels             Allows break statement                  Allows continue statement
+     if         Yes*                               No                                      No
+     while      Yes                                Yes                                     Yes
+     do while   Yes                                Yes                                     Yes
+     for        Yes                                Yes                                     Yes
+     switch     Yes                                Yes                                     No
+
+     **/
 }
