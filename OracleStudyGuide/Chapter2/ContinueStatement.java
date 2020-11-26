ac377f6dbec19b5491bd948a79dd735e3570c2da
David-B-Comer <davec2233@gmail.com>
2020-11-25 22:19:39 -0500

    added continue statement explanation

--- OracleStudyGuide/Chapter2/ContinueStatement.java
+++ OracleStudyGuide/Chapter2/ContinueStatement.java
@@ -1,4 +1,13 @@
 package Chapter2;
 
 public class ContinueStatement {
+    /**
+
+     A statement that causes flow to finish the execution of the current loop.  The continue
+     statement transfers control to the boolean expression that determines if the loop should
+     continue. In other words, it ends the current iteration of the loop. Also like the break
+     statement, the continue statement is applied to the nearest inner loop under execution
+     using optional label statements to override this behavior.
+
+     **/
 }
