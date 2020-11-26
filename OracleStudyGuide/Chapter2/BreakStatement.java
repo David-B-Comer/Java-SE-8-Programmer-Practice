8b27d321cb7a5e10a6a014b4e59adffbe90e134a
David-B-Comer <davec2233@gmail.com>
2020-11-25 21:36:53 -0500

    added break statement explanation

--- OracleStudyGuide/Chapter2/BreakStatement.java
+++ OracleStudyGuide/Chapter2/BreakStatement.java
@@ -1,4 +1,11 @@
 package Chapter2;
 
 public class BreakStatement {
+    /**
+
+     When working with switch statements, a break statement transfers the flow
+     of control out to the enclosing statement. The same holds true for break statements that
+     appear inside of while, do-while, and for loops, as it will end the loop early.
+
+     **/
 }
