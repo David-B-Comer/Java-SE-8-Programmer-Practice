5aebf9558d06f2c882754743a870d63b0ae97252
David-B-Comer <davec2233@gmail.com>
2020-11-25 20:48:23 -0500

    added for each statement explanation

--- OracleStudyGuide/Chapter2/ForEachStatement.java
+++ OracleStudyGuide/Chapter2/ForEachStatement.java
@@ -1,4 +1,16 @@
 package Chapter2;
 
 public class ForEachStatement {
+
+    /**
+
+     Specifically designed for iterating over arrays and Collection objects. The for-each loop declaration is
+     composed of an initialization section and an object to be iterated over. The right-hand side of the for-each loop
+     statement must be a built-in Java array or an object whose class implements java.lang.Iterable, which includes most
+     of the Java Collections framework. The left-hand side of the for-each loop must include a declaration for an
+     instance of a variable, whose type matches the type of a member of the array or collection in the right-hand side
+     of the statement. On each iteration of the loop, the named variable on the left-hand side of the statement is
+     assigned a new value from the array or collection on the right-hand side of the statement.
+
+     **/
 }
