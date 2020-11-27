5c868736c472617951db1b545d70c2e9357eba8e
David-B-Comer <davec2233@gmail.com>
2020-11-27 09:25:09 -0500

    added chapter 2 review question 15

--- OracleStudyGuide/Chapter2/Chapter2ReviewQuestions.java
+++ OracleStudyGuide/Chapter2/Chapter2ReviewQuestions.java
@@ -271,5 +271,23 @@ public class Chapter2ReviewQuestions {
 
 
      Answer C
+
+
+
+
+     15. What is the output of the following code snippet?
+
+     3: int x = 1, y = 15;
+     4: while x < 10
+     5: y––;
+     6: x++;
+     7: System.out.println(x+", "+y);
+
+     A. 10, 5
+     B. 10, 6
+     C. 11, 5
+     D. The code will not compile because of line 3.
+     E. The code will not compile because of line 4.
+     F. The code contains an infinite loop and does not terminate.
      **/
 }
