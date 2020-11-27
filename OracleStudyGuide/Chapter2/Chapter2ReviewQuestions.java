f08c280942e0425485b550808b85f9b1399c3edc
David-B-Comer <davec2233@gmail.com>
2020-11-27 09:42:46 -0500

    added chapter 2 review question 18

--- OracleStudyGuide/Chapter2/Chapter2ReviewQuestions.java
+++ OracleStudyGuide/Chapter2/Chapter2ReviewQuestions.java
@@ -333,5 +333,24 @@ public class Chapter2ReviewQuestions {
 
 
      Answer D
+
+
+
+     18. What is the output of the following code snippet?
+
+     3: int count = 0;
+     4: ROW_LOOP: for(int row = 1; row <=3; row++)
+     5: for(int col = 1; col <=2 ; col++) {
+     6: if(row * col % 2 == 0) continue ROW_LOOP;
+     7: count++;
+     8: }
+     9: System.out.println(count);
+
+     A. 1
+     B. 2
+     C. 3
+     D. 4
+     E. 6
+     F. The code will not compile because of line 6.
      **/
 }
