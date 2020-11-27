dea8b27b07b0300831cf8c0a066dcfa711517015
David-B-Comer <davec2233@gmail.com>
2020-11-27 09:36:44 -0500

    added chapter 2 review question 17

--- OracleStudyGuide/Chapter2/Chapter2ReviewQuestions.java
+++ OracleStudyGuide/Chapter2/Chapter2ReviewQuestions.java
@@ -310,5 +310,25 @@ public class Chapter2ReviewQuestions {
 
 
      Answer D
+
+
+
+     17. What is the output of the following code snippet?
+
+     3: boolean keepGoing = true;
+     4: int result = 15, i = 10;
+     5: do {
+     6: i--;
+     7: if(i==8) keepGoing = false;
+     8: result -= 2;
+     9: } while(keepGoing);
+     10: System.out.println(result);
+
+     A. 7
+     B. 9
+     C. 10
+     D. 11
+     E. 15
+     F. The code will not compile because of line 8.
      **/
 }
