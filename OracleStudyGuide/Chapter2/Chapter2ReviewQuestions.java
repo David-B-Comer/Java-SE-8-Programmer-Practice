ae093f6c5d9b52a4ca2ef5c43c1b4968dba4f8ac
David-B-Comer <davec2233@gmail.com>
2020-11-27 08:52:45 -0500

    added chapter 2 review question 9 answer

--- OracleStudyGuide/Chapter2/Chapter2ReviewQuestions.java
+++ OracleStudyGuide/Chapter2/Chapter2ReviewQuestions.java
@@ -178,5 +178,8 @@ public class Chapter2ReviewQuestions {
      E. The code will not compile because of line 5.
      F. The code contains an infinite loop and does not terminate.
 
+
+     Answer F
+
      **/
 }
