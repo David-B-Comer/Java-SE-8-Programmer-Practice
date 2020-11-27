b45e7cfdd8a767fb0208b515fde2f71ee0bc5e92
David-B-Comer <davec2233@gmail.com>
2020-11-27 09:29:26 -0500

    added chapter 2 review question 16

--- OracleStudyGuide/Chapter2/Chapter2ReviewQuestions.java
+++ OracleStudyGuide/Chapter2/Chapter2ReviewQuestions.java
@@ -292,5 +292,20 @@ public class Chapter2ReviewQuestions {
 
 
      Answer E
+
+
+
+     16. What is the output of the following code snippet?
+
+     3: do {
+     4: int y = 1;
+     5: System.out.print(y++ + " ");
+     6: } while(y <= 10);
+
+     A. 1 2 3 4 5 6 7 8 9
+     B. 1 2 3 4 5 6 7 8 9 10
+     C. 1 2 3 4 5 6 7 8 9 10 11
+     D. The code will not compile because of line 6.
+     E. The code contains an infinite loop and does not terminate.
      **/
 }
