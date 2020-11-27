292a7eef820475b3094b5387455b2f2697ee09a8
David-B-Comer <davec2233@gmail.com>
2020-11-27 09:09:25 -0500

    added chapter 2 review question 12

--- OracleStudyGuide/Chapter2/Chapter2ReviewQuestions.java
+++ OracleStudyGuide/Chapter2/Chapter2ReviewQuestions.java
@@ -217,5 +217,19 @@ public class Chapter2ReviewQuestions {
 
 
      Answer A
+
+
+
+     12. What is the output of the following code snippet?
+
+     3: int x = 0;
+     4: String s = null;
+     5: if(x == s) System.out.println("Success");
+     6: else System.out.println("Failure");
+
+     A. Success
+     B. Failure
+     C. The code will not compile because of line 4.
+     D. The code will not compile because of line 5.
      **/
 }
