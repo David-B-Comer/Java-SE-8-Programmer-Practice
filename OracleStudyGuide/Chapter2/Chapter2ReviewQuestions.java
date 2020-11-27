84a1bdb260a56dbd6ec64cd721a1ad42556008e5
David-B-Comer <davec2233@gmail.com>
2020-11-26 20:29:57 -0500

    added chapter 2 review question 4

--- OracleStudyGuide/Chapter2/Chapter2ReviewQuestions.java
+++ OracleStudyGuide/Chapter2/Chapter2ReviewQuestions.java
@@ -62,5 +62,21 @@ public class Chapter2ReviewQuestions {
 
      Answer F
 
+
+
+
+
+     4. What change would allow the following code snippet to compile? (Choose all that apply)
+
+     3: long x = 10;
+     4: int y = 2 * x;
+
+     A. No change; it compiles as is.
+     B. Cast x on line 4 to int.
+     C. Change the data type of x on line 3 to short.
+     D. Cast 2 * x on line 4 to int.
+     E. Change the data type of y on line 4 to short.
+     F. Change the data type of y on line 4 to long.
+
      **/
 }
