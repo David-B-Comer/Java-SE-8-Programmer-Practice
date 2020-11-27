4db68c7823d0c3d966b822bdde373e10758217de
David-B-Comer <davec2233@gmail.com>
2020-11-26 20:23:58 -0500

    added chapter 2 review question 3

--- OracleStudyGuide/Chapter2/Chapter2ReviewQuestions.java
+++ OracleStudyGuide/Chapter2/Chapter2ReviewQuestions.java
@@ -37,5 +37,27 @@ public class Chapter2ReviewQuestions {
 
      Answer A, B, D
 
+
+
+
+
+     3. What is the output of the following application?
+
+     1: public class CompareValues {
+     2: public static void main(String[] args) {
+     3: int x = 0;
+     4: while(x++ < 10) {}
+     5: String message = x > 10 ? "Greater than" : false;
+     6: System.out.println(message+","+x);
+     7: }
+     8: }
+
+     A. Greater than,10
+     B. false,10
+     C. Greater than,11
+     D. false,11
+     E. The code will not compile because of line 4.
+     F. The code will not compile because of line 5.
+
      **/
 }
