a2569f27dc3062b44343ebf1f4105089b5b55ee6
David-B-Comer <davec2233@gmail.com>
2020-11-27 08:54:19 -0500

    added chapter 2 review question 10

--- OracleStudyGuide/Chapter2/Chapter2ReviewQuestions.java
+++ OracleStudyGuide/Chapter2/Chapter2ReviewQuestions.java
@@ -181,5 +181,19 @@ public class Chapter2ReviewQuestions {
 
      Answer F
 
+
+
+     10. What is the output of the following code?
+
+     3: byte a = 40, b = 50;
+     4: byte sum = (byte) a + b;
+     5: System.out.println(sum);
+
+     A. 40
+     B. 50
+     C. 90
+     D. The code will not compile because of line 4.
+     E. An undefined value.
+
      **/
 }
