4cc9de49b4ff9f9eb63dc7ba9965a5a3c6663a8c
David-B-Comer <davec2233@gmail.com>
2020-11-27 08:49:54 -0500

    added chapter 2 review question 9

--- OracleStudyGuide/Chapter2/Chapter2ReviewQuestions.java
+++ OracleStudyGuide/Chapter2/Chapter2ReviewQuestions.java
@@ -161,5 +161,22 @@ public class Chapter2ReviewQuestions {
 
 
      Answer B
+
+
+
+     9. How many times will the following code print "Hello World"?
+
+     3: for(int i=0; i<10 ; ) {
+     4: i = i++;
+     5: System.out.println("Hello World");
+     6: }
+
+     A. 9
+     B. 10
+     C. 11
+     D. The code will not compile because of line 3.
+     E. The code will not compile because of line 5.
+     F. The code contains an infinite loop and does not terminate.
+
      **/
 }
