bcf9043841c99ac97c82693c2c22ca03f2c6c3e7
David-B-Comer <davec2233@gmail.com>
2020-11-27 09:55:15 -0500

    added chapter 2 review question 20

--- OracleStudyGuide/Chapter2/Chapter2ReviewQuestions.java
+++ OracleStudyGuide/Chapter2/Chapter2ReviewQuestions.java
@@ -378,5 +378,25 @@ public class Chapter2ReviewQuestions {
 
 
      Answer D
+
+
+
+     20. What is the result of the following code snippet?
+
+     3: final char a = 'A', d = 'D';
+     4: char grade = 'B';
+     5: switch(grade) {
+     6: case a:
+     7: case 'B': System.out.print("great");
+     8: case 'C': System.out.print("good"); break;
+     9: case d:
+     10: case 'F': System.out.print("not good");
+     11: }
+
+     A. great
+     B. greatgood
+     C. The code will not compile because of line 3.
+     D. The code will not compile because of line 6.
+     E. The code will not compile because of lines 6 and 9.
      **/
 }
