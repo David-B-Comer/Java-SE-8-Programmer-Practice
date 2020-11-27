4566de293c9d75725ca7f71f12daa2cbff2f8744
David-B-Comer <davec2233@gmail.com>
2020-11-27 09:02:17 -0500

    added chapter 2 review question 11

--- OracleStudyGuide/Chapter2/Chapter2ReviewQuestions.java
+++ OracleStudyGuide/Chapter2/Chapter2ReviewQuestions.java
@@ -197,5 +197,23 @@ public class Chapter2ReviewQuestions {
 
 
      Answer D
+
+
+
+
+     11. What is the output of the following code?
+
+     1: public class ArithmeticSample {
+     2: public static void main(String[] args) {
+     3: int x = 5 * 4 % 3;
+     4: System.out.println(x);
+     5: }}
+
+     A. 2
+     B. 3
+     C. 5
+     D. 6
+     E. The code will not compile because of line 3.
+
      **/
 }
