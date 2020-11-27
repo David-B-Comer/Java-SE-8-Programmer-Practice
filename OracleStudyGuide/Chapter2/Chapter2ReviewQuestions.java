bca3f83af96aaf542e458486d55b8aa84798f210
David-B-Comer <davec2233@gmail.com>
2020-11-26 20:09:46 -0500

    added chapter 2 review question 1 answer

--- OracleStudyGuide/Chapter2/Chapter2ReviewQuestions.java
+++ OracleStudyGuide/Chapter2/Chapter2ReviewQuestions.java
@@ -6,12 +6,16 @@ public class Chapter2ReviewQuestions {
 
      1. Which of the following Java operators can be used with boolean variables? (Choose all that
      apply)
+
      A. ==
      B. +
      C. --
      D. !
      E. %
      F. <=
+
+
+     Answer A, D
 
      **/
 }
