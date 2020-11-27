e406db284c58e9e491fbe780b6bbc7574220fe24
David-B-Comer <davec2233@gmail.com>
2020-11-27 08:30:35 -0500

    added chapter 2 review question 6

--- OracleStudyGuide/Chapter2/Chapter2ReviewQuestions.java
+++ OracleStudyGuide/Chapter2/Chapter2ReviewQuestions.java
@@ -104,5 +104,22 @@ public class Chapter2ReviewQuestions {
 
      Answer C
 
+
+
+     6. What is the output of the following code snippet?
+
+     3: int x = 4;
+     4: long y = x * 4 - x++;
+     5: if(y<10) System.out.println("Too Low");
+     6: else System.out.println("Just right");
+     7: else System.out.println("Too High");
+
+     A. Too Low
+     B. Just Right
+     C. Too High
+     D. Compiles but throws a NullPointerException.
+     E. The code will not compile because of line 6.
+     F. The code will not compile because of line 7.
+
      **/
 }
