760fad8692bfadfb1c196b494975523bd7ca2e13
David-B-Comer <davec2233@gmail.com>
2020-11-26 20:34:29 -0500

    added chapter 2 review question 5

--- OracleStudyGuide/Chapter2/Chapter2ReviewQuestions.java
+++ OracleStudyGuide/Chapter2/Chapter2ReviewQuestions.java
@@ -81,5 +81,25 @@ public class Chapter2ReviewQuestions {
 
      Answer B, C, D, F
 
+
+
+
+     5. What is the output of the following code snippet?
+
+     3: java.util.List<Integer> list = new java.util.ArrayList<Integer>();
+     4: list.add(10);
+     5: list.add(14);
+     6: for(int x : list) {
+     7: System.out.print(x + ", ");
+     8: break;
+     9: }
+
+     A. 10, 14,
+     B. 10, 14
+     C. 10,
+     D. The code will not compile because of line 7.
+     E. The code will not compile because of line 8.
+     F. The code contains an infinite loop and does not terminate.
+
      **/
 }
