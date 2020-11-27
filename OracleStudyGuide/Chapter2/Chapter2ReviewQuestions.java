57ccceeedf64710f1744ab5340e53fbcff7cc418
David-B-Comer <davec2233@gmail.com>
2020-11-27 09:17:32 -0500

    added chapter 2 review question 13

--- OracleStudyGuide/Chapter2/Chapter2ReviewQuestions.java
+++ OracleStudyGuide/Chapter2/Chapter2ReviewQuestions.java
@@ -234,5 +234,19 @@ public class Chapter2ReviewQuestions {
 
 
      Answer D
+
+
+
+     13. What is the output of the following code snippet?
+
+     3: int x1 = 50, x2 = 75;
+     4: boolean b = x1 >= x2;
+     5: if(b = true) System.out.println("Success");
+     6: else System.out.println("Failure");
+
+     A. Success
+     B. Failure
+     C. The code will not compile because of line 4.
+     D. The code will not compile because of line 5.
      **/
 }
