f000b56dc21c54bff289df0a65ee42918a7c0209
David-B-Comer <davec2233@gmail.com>
2020-11-28 21:34:55 -0500

    added concatenation explanation

--- OracleStudyGuide/Chapter3/Concatenation.java
+++ OracleStudyGuide/Chapter3/Concatenation.java
@@ -1,4 +1,16 @@
 package Chapter3;
 
 public class Concatenation {
+    /**
+
+     1 + 2 is clearly 3. But what is "1" + "2"? It’s actually "12" because Java combines the two String objects.
+     Placing one String before the other String and combining them together is called string
+     concatenation. The OCA exam creators like string concatenation because the + operator
+     can be used in two ways within the same line of code. There aren’t a lot of rules to know for
+     this, but you have to know them well:
+     1. If both operands are numeric, + means numeric addition.
+     2. If either operand is a String, + means concatenation.
+     3. The expression is evaluated left to right.
+
+     **/
 }
