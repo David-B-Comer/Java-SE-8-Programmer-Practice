549cd1e162c7af1299b9f233bad4c8931c9f3055
David-B-Comer <davec2233@gmail.com>
2020-11-28 21:52:04 -0500

    added important string methods explanation

--- OracleStudyGuide/Chapter3/ImportantStringMethods.java
+++ OracleStudyGuide/Chapter3/ImportantStringMethods.java
@@ -1,4 +1,15 @@
 package Chapter3;
 
 public class ImportantStringMethods {
+    /**
+
+     The exam creators pick most of the methods developers use in the real world.
+     For all these methods, you need to remember that a string is a sequence of characters
+     and Java counts from 0 when indexed.
+
+
+
+
+
+     **/
 }
