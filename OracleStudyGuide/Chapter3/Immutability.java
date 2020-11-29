596fd2050bbfe02938d4c09a7e5ded669aa93c40
David-B-Comer <davec2233@gmail.com>
2020-11-28 21:39:46 -0500

    added immutability example

--- OracleStudyGuide/Chapter3/Immutability.java
+++ OracleStudyGuide/Chapter3/Immutability.java
@@ -12,5 +12,23 @@ public class Immutability {
     can’t be changed once it’s created. On the OCA exam, you need to know that String is
     immutable.
 
+
+     class Mutable {
+     private String s;
+     public void setS(String newS){ s = newS; } // Setter makes it mutable
+     public String getS() { return s; }
+     }
+     final class Immutable {
+     private String s = "name";
+     public String getS() { return s; }
+     }
+
+     Immutable only has a getter. There's no way to change the value of s once it's set.
+     Mutable has a setter as well. This allows the reference s to change to point to a different
+     String later. Note that even though the String class is immutable, it can still be used in
+     a mutable class. You can even make the instance variable final so the compiler reminds
+     you if you accidentally change s.
+     Also, immutable classes in Java are final, and subclasses can’t add mutable behavior.
+
      **/
 }
