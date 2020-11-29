07d1cf521439faf34251654c53387364bf186fb3
David-B-Comer <davec2233@gmail.com>
2020-11-28 21:38:40 -0500

    added immutability explanation

--- OracleStudyGuide/Chapter3/Immutability.java
+++ OracleStudyGuide/Chapter3/Immutability.java
@@ -1,4 +1,16 @@
 package Chapter3;
 
 public class Immutability {
+    /**
+
+    Once a String object is created, it is not allowed to change. It cannot be made larger or
+    smaller, and you cannot change one of the characters inside it.
+    You can think of a string as a storage box you have perfectly full and whose sides can’t
+    bulge. There’s no way to add objects, nor can you replace objects without disturbing the
+    entire arrangement. The trade-off for the optimal packing is zero fl exibility.
+    Mutable is another word for changeable. Immutable is the opposite—an object that
+    can’t be changed once it’s created. On the OCA exam, you need to know that String is
+    immutable.
+
+     **/
 }
