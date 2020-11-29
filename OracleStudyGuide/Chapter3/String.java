dbf71ac711c6caf56f2db72e69488483eba2e636
David-B-Comer <davec2233@gmail.com>
2020-11-28 21:26:38 -0500

    added string examples

--- OracleStudyGuide/Chapter3/String.java
+++ OracleStudyGuide/Chapter3/String.java
@@ -1,4 +1,13 @@
 package Chapter3;
 
 public class String {
+    /**
+
+     String name = "Fluffy";
+     String name = new String("Fluffy");
+
+     Both create the same reference variable of type name pointing to the String object "Fluffy".  For now, just
+     remember that the String class is special and doesn’t need to be instantiated with new.
+
+     **/
 }
