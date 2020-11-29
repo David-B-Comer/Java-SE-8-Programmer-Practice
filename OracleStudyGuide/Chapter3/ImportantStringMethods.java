6b1d049acd03c661483114cc4f6f041769acd836
David-B-Comer <davec2233@gmail.com>
2020-11-28 21:53:38 -0500

    added important string methods length

--- OracleStudyGuide/Chapter3/ImportantStringMethods.java
+++ OracleStudyGuide/Chapter3/ImportantStringMethods.java
@@ -9,6 +9,16 @@ public class ImportantStringMethods {
 
 
 
+     length()
+     The method length() returns the number of characters in the String. The method signature is as follows:
+     int length()
+     The following code shows how to use length():
+     String string = "animals";
+     System.out.println(string.length()); // 7
+     Wait. It outputs 7? Didn’t we just tell you that Java counts from 0? The difference is
+     that zero counting happens only when you’re using indexes or positions within a list. When
+     determining the total size or length, Java uses normal counting again.
+
 
 
      **/
