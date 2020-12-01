0732ab366b9b12a491ce1bf91453178f18335ed8
David-B-Comer <davec2233@gmail.com>
2020-12-01 18:34:54 -0500

    added creating multidimensional arrays

--- OracleStudyGuide/Chapter3/MultidimensionalArrays.java
+++ OracleStudyGuide/Chapter3/MultidimensionalArrays.java
@@ -1,4 +1,22 @@
 package Chapter3;
 
 public class MultidimensionalArrays {
+    /**
+
+     Arrays are objects, and of course array components can be objects. It doesn’t take much
+     time, rubbing those two facts together, to wonder if arrays can hold other arrays, and of
+     course they can.
+
+
+
+
+     Creating a Multidimensional Array
+     Multiple array separators are all it takes to declare arrays with multiple dimensions. You
+     can locate them with the type or variable name in the declaration, just as before:
+     int[][] vars1; // 2D array
+     int vars2 [][]; // 2D array
+     int[] vars3[]; // 2D array
+     int[] vars4 [], space [][]; // a 2D AND a 3D array
+
+     **/
 }
