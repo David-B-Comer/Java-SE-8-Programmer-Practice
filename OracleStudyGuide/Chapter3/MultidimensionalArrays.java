d8095a0fc4dc94f8a72febc061cd1a3c9b7da2ca
David-B-Comer <davec2233@gmail.com>
2020-12-01 18:38:45 -0500

    added creating multidimensional arrays explanation

--- OracleStudyGuide/Chapter3/MultidimensionalArrays.java
+++ OracleStudyGuide/Chapter3/MultidimensionalArrays.java
@@ -18,5 +18,18 @@ public class MultidimensionalArrays {
      int[] vars3[]; // 2D array
      int[] vars4 [], space [][]; // a 2D AND a 3D array
 
+     The first two examples are nothing surprising and declare a two-dimensional (2D) array.
+     The third example also declares a 2D array. There’s no good reason to use this style other
+     than to confuse readers of your code. The fi nal example declares two arrays on the same
+     line. Adding up the brackets, we see that the vars4 is a 2D array and space is a 3D array.
+     Again, there’ no reason to use this style other than to confuse readers of your code. The
+     exam creators like to try to confuse you, though. Luckily you are on to them and won’t let
+     this happen to you!
+     You can specify the size of your multidimensional array in the declaration if you like:
+     String [][] rectangle = new String[3][2];
+     The result of this statement is an array rectangle with three elements, each of which
+     refers to an array of two elements. You can think of the addressable range as [0][0] through
+     [2][1], but don’t think of it as a structure of addresses like [0,0] or [2,1].
+
      **/
 }
