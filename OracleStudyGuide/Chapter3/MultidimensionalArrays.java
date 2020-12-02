e140f53073ba720d73958d60eff353532e976680
David-B-Comer <davec2233@gmail.com>
2020-12-01 19:13:29 -0500

    added using multidimensional arrays explanation

--- OracleStudyGuide/Chapter3/MultidimensionalArrays.java
+++ OracleStudyGuide/Chapter3/MultidimensionalArrays.java
@@ -44,5 +44,18 @@ public class MultidimensionalArrays {
      System.out.println(); // time for a new row
      }
 
+     We have two loops here. The fi rst uses index i and goes through the fi rst subarray for
+     twoD. The second uses a different loop variable j. It is very important these be different
+     variable names so the loops don’t get mixed up. The inner loop looks at how many elements
+     are in the second-level array. The inner loop prints the element and leaves a space for readability. When the inner loop completes, the outer loop goes to a new line and repeats the
+     process for the next element.
+     This entire exercise would be easier to read with the enhanced for loop.
+     for (int[] inner : twoD) {
+     for (int num : inner)
+     System.out.print(num + " ");
+     System.out.println();
+     }
+     We’ll grant you that it isn’t fewer lines, but each line is less complex and there aren’t any
+     loop variables or terminating conditions to mix up.
      **/
 }
