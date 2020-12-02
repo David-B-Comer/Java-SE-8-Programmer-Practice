3175d7ff880446a0026dfbeeebb789a27fefad3f
David-B-Comer <davec2233@gmail.com>
2020-12-01 21:44:00 -0500

    added wrapper classes table

--- OracleStudyGuide/Chapter3/WrapperClasses.java
+++ OracleStudyGuide/Chapter3/WrapperClasses.java
@@ -1,4 +1,22 @@
 package Chapter3;
 
 public class WrapperClasses {
+    /**
+
+     Up to now, we’ve only put String objects in the ArrayList. What happens if we want to
+     put primitives in? Each primitive type has a wrapper class, which is an object type that
+     corresponds to the primitive.
+
+     Wrapper classes
+     Primitive type                         Wrapper class                   Example of constructing
+     boolean                                Boolean                         new Boolean(true)
+     byte                                   Byte                            new Byte((byte) 1)
+     short                                  Short                           new Short((short) 1)
+     int                                    Integer                         new Integer(1)
+     long                                   Long                            new Long(1)
+     float                                  Float                           new Float(1.0)
+     double                                 Double                          new Double(1.0)
+     char                                   Character                       new Character('c')
+
+     **/
 }
