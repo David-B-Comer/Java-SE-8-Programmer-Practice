92da705e03dbea4db13546ab38aced8fac5e3d79
David-B-Comer <davec2233@gmail.com>
2020-12-01 19:18:29 -0500

    added understanding an arraylist

--- OracleStudyGuide/Chapter3/Arraylist.java
+++ OracleStudyGuide/Chapter3/Arraylist.java
@@ -1,4 +1,24 @@
 package Chapter3;
 
 public class Arraylist {
+    /**
+
+     Understanding an ArrayList
+     An array has one glaring shortcoming: you have to know how many elements will be in the
+     array when you create it and then you are stuck with that choice. Just like a StringBuilder,
+     ArrayList can change size at runtime as needed. Like an array, an ArrayList is an ordered
+     sequence that allows duplicates.
+     As when we used Arrays.sort, ArrayList requires an import. To use it, you must have
+     either of the following two statements in your class:
+     import java.util.* // import whole package including ArrayList
+     import java.util.ArrayList; // import just ArrayList
+     Remember that if you are shown a code snippet with a line number that doesn’t begin
+     with 1, you can assume the necessary imports are there. Similarly, you can assume the
+     imports are present if you are shown a snippet of a method.
+     In this section, we’ll look at creating an ArrayList, common methods, autoboxing,
+     conversion, and sorting.
+     Experienced programmers, take note: This section is simplified and doesn’t cover a
+     number of topics that are out of scope for the OCA exam.
+
+     **/
 }
