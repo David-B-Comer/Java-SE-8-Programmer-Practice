2a1a638597c6dd00be01bf0c79cf346cdd1f76d5
David-B-Comer <davec2233@gmail.com>
2020-12-02 08:31:14 -0500

    added working with dates and times imports

--- OracleStudyGuide/Chapter3/WorkingWithDatesAndTimes.java
+++ OracleStudyGuide/Chapter3/WorkingWithDatesAndTimes.java
@@ -1,4 +1,17 @@
 package Chapter3;
 
 public class WorkingWithDatesAndTimes {
+    /**
+
+     In Java 8, Oracle completely revamped how we work with dates and times. You can still
+     write code the “old way,” but those classes aren’t on the exam. We’ll mention the “old way”
+     in real-world scenarios so that you can learn the “new way” more easily if you fi rst learned
+     Java before version 8. Even if you are learning Java starting with version 8, this will help
+     you when you need to read older code. Just know that the “old way” is not on the exam.
+     As with an ArrayList, you need an import statement to work with the date and time classes.
+     Most of them are in the java.time package. To use it, add this import to your program:
+     import java.time.*; // import time classes
+     In the following sections, we’ll look at creating, manipulating, and formatting dates and times.
+
+     **/
 }
