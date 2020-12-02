5be3171f8e67e67cf48e750dbd189659aeb43bae
David-B-Comer <davec2233@gmail.com>
2020-12-01 19:25:44 -0500

    added creating an arraylist

--- OracleStudyGuide/Chapter3/Arraylist.java
+++ OracleStudyGuide/Chapter3/Arraylist.java
@@ -20,5 +20,36 @@ public class Arraylist {
      Experienced programmers, take note: This section is simplified and doesn’t cover a
      number of topics that are out of scope for the OCA exam.
 
+
+
+
+
+     Creating an ArrayList
+     As with StringBuilder, there are three ways to create an ArrayList:
+     ArrayList list1 = new ArrayList();
+     ArrayList list2 = new ArrayList(10);
+     ArrayList list3 = new ArrayList(list2);
+     The first says to create an ArrayList containing space for the default number of
+     elements but not to fi ll any slots yet. The second says to create an ArrayList containing a
+     specific number of slots, but again not to assign any. The fi nal example tells Java that we
+     want to make a copy of another ArrayList. We copy both the size and contents of that
+     ArrayList. Granted, list2 is empty in this example so it isn’t particularly interesting.
+     Although these are the only three constructors you need to know, you do need to learn
+     some variants of it. The previous examples were the old pre–Java 5 way of creating an
+     ArrayList. They still work and you still need to know they work. You also need to know
+     the new and improved way. Java 5 introduced generics, which allow you to specify the type
+     of class that the ArrayList will contain.
+     ArrayList<String> list4 = new ArrayList<String>();
+     ArrayList<String> list5 = new ArrayList<>();
+     Java 5 allows you to tell the compiler what the type would be by specifying it between <
+     and >. Starting in Java 7, you can even omit that type from the right side. The < and > are
+     still required, though. This is called the diamond operator because <> looks like a diamond.
+     Just when you thought you knew everything about creating an ArrayList, there is one
+     more thing you need to know. ArrayList implements an interface called List. In other
+     words, an ArrayList is a List. You will learn about interfaces in Chapter 5. In the meantime, just know that you can store an ArrayList in a List reference variable but not vice
+     versa. The reason is that List is an interface and interfaces can’t be instantiated.
+     List<String> list6 = new ArrayList<>();
+     ArrayList<String> list7 = new List<>(); // DOES NOT COMPILE
+
      **/
 }
