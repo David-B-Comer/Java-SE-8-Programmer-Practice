ba4bee3cc8e011d031024830b4f3eb7c143b1a70
David-B-Comer <davec2233@gmail.com>
2020-12-03 21:04:14 -0500

    added chapter 3 review question 4

--- OracleStudyGuide/Chapter3/Chapter3ReviewQuestions.java
+++ OracleStudyGuide/Chapter3/Chapter3ReviewQuestions.java
@@ -64,5 +64,22 @@ public class Chapter3ReviewQuestions {
 
      Answer B, C, E
 
+
+
+
+
+     4. What is the result of the following code?
+
+     7: StringBuilder sb = new StringBuilder();
+     8: sb.append("aaa").insert(1, "bb").insert(4, "ccc");
+     9: System.out.println(sb);
+
+     A. abbaaccc
+     B. abbaccca
+     C. bbaaaccc
+     D. bbaaccca
+     E. An exception is thrown.
+     F. The code does not compile.
+
      **/
 }
