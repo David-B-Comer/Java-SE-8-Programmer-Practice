50a259ba702f4bd36624b8ca06127cdaccb1fe0b
David-B-Comer <davec2233@gmail.com>
2020-12-04 10:26:03 -0500

    added chapter 3 review question 25

--- OracleStudyGuide/Chapter3/Chapter3ReviewQuestions.java
+++ OracleStudyGuide/Chapter3/Chapter3ReviewQuestions.java
@@ -488,5 +488,25 @@ public class Chapter3ReviewQuestions {
 
      Answer C
 
+
+
+
+
+     25. What is the result of the following?
+
+     List<String> hex = Arrays.asList("30", "8", "3A", "FF");
+     Collections.sort(hex);
+     int x = Collections.binarySearch(hex, "8");
+     int y = Collections.binarySearch(hex, "3A");
+     int z = Collections.binarySearch(hex, "4F");
+     System.out.println(x + " " + y + " " + z);
+
+     A 0 1 –2
+     B. 0 1 –3
+     C. 2 1 –2
+     D. 2 1 –3
+     E. None of the above.
+     F. The code doesn’t compile.
+
      **/
 }
