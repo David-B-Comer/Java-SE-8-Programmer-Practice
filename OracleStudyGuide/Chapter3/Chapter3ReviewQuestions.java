0c0e5b7deaedc8d0ec24729a11ecb1144d13bdac
David-B-Comer <davec2233@gmail.com>
2020-12-04 10:59:57 -0500

    added chapter 3 review question 31

--- OracleStudyGuide/Chapter3/Chapter3ReviewQuestions.java
+++ OracleStudyGuide/Chapter3/Chapter3ReviewQuestions.java
@@ -625,5 +625,25 @@ public class Chapter3ReviewQuestions {
 
      Answer F
 
+
+
+
+
+     31. What is the output of the following code?
+
+     LocalDate date = LocalDate.of(2018, Month.APRIL, 30);
+     date.plusDays(2);
+     date.plusYears(3);
+     System.out.println(date.getYear() + " " + date.getMonth() + " "
+     + date.getDayOfMonth());
+
+     A. 2018 APRIL 2
+     B. 2018 APRIL 30
+     C. 2018 MAY 2
+     D. 2021 APRIL 2
+     E. 2021 APRIL 30
+     F. 2021 MAY 2
+     G. A runtime exception is thrown.
+
      **/
 }
