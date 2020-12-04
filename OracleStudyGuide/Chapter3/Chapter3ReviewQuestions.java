403d68a5f884bb139a877de72abe27eef4aeab19
David-B-Comer <davec2233@gmail.com>
2020-12-04 10:11:45 -0500

    added chapter 3 review question 21

--- OracleStudyGuide/Chapter3/Chapter3ReviewQuestions.java
+++ OracleStudyGuide/Chapter3/Chapter3ReviewQuestions.java
@@ -403,5 +403,26 @@ public class Chapter3ReviewQuestions {
 
      Answer D
 
+
+
+
+
+     21. What is the result of the following statements?
+
+     3: ArrayList<Integer> values = new ArrayList<>();
+     4: values.add(4);
+     5: values.add(5);
+     6: values.set(1, 6);
+     7: values.remove(0);
+     8: for (Integer v : values) System.out.print(v);
+
+     A. 4
+     B. 5
+     C. 6
+     D. 46
+     E. 45
+     F. An exception is thrown.
+     G. The code does not compile.
+
      **/
 }
