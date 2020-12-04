9826cb025496d3ea5eaa4cb51b3c33df126e7edc
David-B-Comer <davec2233@gmail.com>
2020-12-03 21:20:54 -0500

    added chapter 3 review question 9

--- OracleStudyGuide/Chapter3/Chapter3ReviewQuestions.java
+++ OracleStudyGuide/Chapter3/Chapter3ReviewQuestions.java
@@ -174,5 +174,24 @@ public class Chapter3ReviewQuestions {
 
      Answer A, D, E
 
+
+
+
+     9. What is the result of the following code?
+
+     3: String s = "purr";
+     4: s.toUpperCase();
+     5: s.trim();
+     6: s.substring(1, 3);
+     7: s += " two";
+     8: System.out.println(s.length());
+
+     A. 2
+     B. 4
+     C. 8
+     D. 10
+     E. An exception is thrown.
+     F. The code does not compile.
+
      **/
 }
