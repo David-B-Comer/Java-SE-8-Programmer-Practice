ec2e419f92d4434aabd6190b54be2457ee30af07
David-B-Comer <davec2233@gmail.com>
2020-12-04 10:23:41 -0500

    added chapter 3 review question 24

--- OracleStudyGuide/Chapter3/Chapter3ReviewQuestions.java
+++ OracleStudyGuide/Chapter3/Chapter3ReviewQuestions.java
@@ -468,5 +468,22 @@ public class Chapter3ReviewQuestions {
 
      Answer A
 
+
+
+
+
+     24. What is the result of the following?
+
+     6: String [] names = {"Tom", "Dick", "Harry"};
+     7: List<String> list = names.asList();
+     8: list.set(0, "Sue");
+     9: System.out.println(names[0]);
+
+     A. Sue
+     B. Tom
+     C. Compiler error on line 7.
+     D. Compiler error on line 8.
+     E. An exception is thrown.
+
      **/
 }
