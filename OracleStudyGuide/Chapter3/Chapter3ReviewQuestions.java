b77b9345977b93bcce0dbdff162b49cbf7e1269c
David-B-Comer <davec2233@gmail.com>
2020-12-04 09:46:15 -0500

    added chapter 3 review question 16

--- OracleStudyGuide/Chapter3/Chapter3ReviewQuestions.java
+++ OracleStudyGuide/Chapter3/Chapter3ReviewQuestions.java
@@ -314,5 +314,22 @@ public class Chapter3ReviewQuestions {
 
      Answer C, E, F
 
+
+
+
+
+     16. Which of these compile when replacing line 8? (Choose all that apply)
+
+     7: char[]c = new char[2];
+     8: // INSERT CODE HERE
+
+     A. int length = c.capacity;
+     B. int length = c.capacity();
+     C. int length = c.length;
+     D. int length = c.length();
+     E. int length = c.size;
+     F. int length = c.size();
+     G. None of the above.
+
      **/
 }
