2397d24f03a30c900bd2fe9c5eaf4071bdd59673
David-B-Comer <davec2233@gmail.com>
2020-12-04 10:05:57 -0500

    added chapter 3 review question 20

--- OracleStudyGuide/Chapter3/Chapter3ReviewQuestions.java
+++ OracleStudyGuide/Chapter3/Chapter3ReviewQuestions.java
@@ -383,5 +383,22 @@ public class Chapter3ReviewQuestions {
 
      Answer B, C
 
+
+
+
+     20. What is the result of the following statements?
+
+     6: List<String> list = new ArrayList<String>();
+     7: list.add("one");
+     8: list.add("two");
+     9: list.add(7);
+     10: for(String s : list) System.out.print(s);
+
+     A. onetwo
+     B. onetwo7
+     C. onetwo followed by an exception
+     D. Compiler error on line 9.
+     E. Compiler error on line 10.
+
      **/
 }
