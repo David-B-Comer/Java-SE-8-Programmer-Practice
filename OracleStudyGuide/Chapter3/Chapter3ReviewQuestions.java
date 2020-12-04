a3530c0d6e3f87313e252ed56bbcc893c111a02e
David-B-Comer <davec2233@gmail.com>
2020-12-04 10:45:01 -0500

    added chapter 3 review question 28

--- OracleStudyGuide/Chapter3/Chapter3ReviewQuestions.java
+++ OracleStudyGuide/Chapter3/Chapter3ReviewQuestions.java
@@ -559,5 +559,26 @@ public class Chapter3ReviewQuestions {
 
      Answer B
 
+
+
+
+
+     28. Which of the following can be inserted into the blank to create a date of June 21, 2014?
+     (Choose all that apply)
+
+     import java.time.*;
+     public class StartOfSummer {
+     public static void main(String[] args) {
+     LocalDate date = ____________________________
+     }
+     }
+
+     A. new LocalDate(2014, 5, 21);
+     B. new LocalDate(2014, 6, 21);
+     C. LocalDate.of(2014, 5, 21);
+     D. LocalDate.of(2014, 6, 21);
+     E. LocalDate.of(2014, Calendar.JUNE, 21);
+     F. LocalDate.of(2014, Month.JUNE, 21);
+
      **/
 }
