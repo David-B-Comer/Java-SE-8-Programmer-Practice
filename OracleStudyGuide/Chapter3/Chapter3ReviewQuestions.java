f1eb26b3e06c906775e5c7aa7bbccb0c9d140c7a
David-B-Comer <davec2233@gmail.com>
2020-12-04 10:20:23 -0500

    added chapter 3 review question 23

--- OracleStudyGuide/Chapter3/Chapter3ReviewQuestions.java
+++ OracleStudyGuide/Chapter3/Chapter3ReviewQuestions.java
@@ -447,5 +447,24 @@ public class Chapter3ReviewQuestions {
 
      Answer D
 
+
+
+
+
+     23. What is the result of the following?
+
+     4: List<Integer> list = Arrays.asList(10, 4, -1, 5);
+     5: Collections.sort(list);
+     6: Integer array[] = list.toArray(new Integer[4]);
+     7: System.out.println(array[0]);
+
+     A. –1
+     B. 10
+     C. Compiler error on line 4.
+     D. Compiler error on line 5.
+     E. Compiler error on line 6.
+     F. An exception is thrown.
+
+
      **/
 }
