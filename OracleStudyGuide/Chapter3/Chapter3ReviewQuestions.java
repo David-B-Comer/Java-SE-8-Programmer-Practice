60406bc8b632c94cf263ff0a157f4af026377c95
David-B-Comer <davec2233@gmail.com>
2020-12-03 21:14:03 -0500

    added chapter 3 review question 7

--- OracleStudyGuide/Chapter3/Chapter3ReviewQuestions.java
+++ OracleStudyGuide/Chapter3/Chapter3ReviewQuestions.java
@@ -133,5 +133,22 @@ public class Chapter3ReviewQuestions {
 
      Answer B
 
+
+
+
+     7. Which are the results of the following code? (Choose all that apply)
+
+     String letters = "abcdef";
+     System.out.println(letters.length());
+     System.out.println(letters.charAt(3));
+     System.out.println(letters.charAt(6));
+
+     A. 5
+     B. 6
+     C. c
+     D. d
+     E. An exception is thrown.
+     F. The code does not compile.
+
      **/
 }
