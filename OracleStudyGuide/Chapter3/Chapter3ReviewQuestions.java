7e79b41d8403dadf4f0c8e1bb5e4f3c08500ff17
David-B-Comer <davec2233@gmail.com>
2020-12-03 21:11:52 -0500

    added chapter 3 review question 6

--- OracleStudyGuide/Chapter3/Chapter3ReviewQuestions.java
+++ OracleStudyGuide/Chapter3/Chapter3ReviewQuestions.java
@@ -106,5 +106,29 @@ public class Chapter3ReviewQuestions {
 
      Answer F
 
+
+
+
+     6. What is the result of the following code?
+
+     public class Lion {
+     public void roar(String roar1, StringBuilder roar2) {
+     roar1.concat("!!!");
+     roar2.append("!!!");
+     }
+     public static void main(String[] args) {
+     String roar1 = "roar";
+     StringBuilder roar2 = new StringBuilder("roar");
+     new Lion().roar(roar1, roar2);
+     System.out.println(roar1 + " " + roar2);
+     } }
+
+     A. roar roar
+     B. roar roar!!!
+     C. roar!!! roar
+     D. roar!!! roar!!!
+     E. An exception is thrown.
+     F. The code does not compile.
+
      **/
 }
