685153ca24ac372091e984f7992672d0876ab460
David-B-Comer <davec2233@gmail.com>
2020-12-03 20:02:53 -0500

    added chapter 3 review question 1

--- OracleStudyGuide/Chapter3/Chapter3ReviewQuestions.java
+++ OracleStudyGuide/Chapter3/Chapter3ReviewQuestions.java
@@ -1,4 +1,25 @@
 package Chapter3;
 
 public class Chapter3ReviewQuestions {
+    /**
+
+     1. What is output by the following code? (Choose all that apply)
+     1: public class Fish {
+     2: public static void main(String[] args) {
+     3: int numFish = 4;
+     4: String fishType = "tuna";
+     5: String anotherFish = numFish + 1;
+     6: System.out.println(anotherFish + " " + fishType);
+     7: System.out.println(numFish + " " + 1);
+     8: } }
+
+     A. 4 1
+     B. 41
+     C. 5
+     D. 5 tuna
+     E. 5tuna
+     F. 51tuna
+     G. The code does not compile.
+
+     **/
 }
