f1c87339efce32e3a4be8686d34ff1e276689e9d
David-B-Comer <davec2233@gmail.com>
2020-12-03 21:32:02 -0500

    added chapter 3 review question 12

--- OracleStudyGuide/Chapter3/Chapter3ReviewQuestions.java
+++ OracleStudyGuide/Chapter3/Chapter3ReviewQuestions.java
@@ -242,5 +242,22 @@ public class Chapter3ReviewQuestions {
 
      Answer E
 
+
+
+
+     12. What is the result of the following code? (Choose all that apply)
+
+     StringBuilder numbers = new StringBuilder("0123456789");
+     numbers.delete(2, 8);
+     numbers.append("-").insert(2, "+");
+     System.out.println(numbers);
+
+     A. 01+89–
+     B. 012+9–
+     C. 012+–9
+     D. 0123456789
+     E. An exception is thrown.
+     F. The code does not compile.
+
      **/
 }
