7366ab79a841be9e53ccda94a4b7820f22a06310
David-B-Comer <davec2233@gmail.com>
2020-12-05 22:53:32 -0500

    added access modifiers

--- OracleStudyGuide/Chapter4/DesigningMethods.java
+++ OracleStudyGuide/Chapter4/DesigningMethods.java
@@ -19,5 +19,23 @@ public class DesigningMethods {
      Optional exception list    throws InterruptedException         No
      Method body                {// take a nap }                    Yes, but can be empty brace
 
+
+
+
+
+     Access Modifiers
+     Java offers four choices of access modifier:
+
+     public The method can be called from any class.
+
+     private The method can only be called from within the same class.
+
+     protected The method can only be called from classes in the same package or subclasses.
+     You’ll learn about subclasses in Chapter 5.
+
+     Default (Package Private) Access The method can only be called from classes in the same
+     package. This one is tricky because there is no keyword for default access. You simply omit
+     the access modifier
+
      **/
 }
