5145b8f3ec727257e779d200d466038599aa832e
David-B-Comer <davec2233@gmail.com>
2020-12-06 18:49:26 -0500

    added applying access modifiers explanation

--- OracleStudyGuide/Chapter4/ApplyingAccessModifiers.java
+++ OracleStudyGuide/Chapter4/ApplyingAccessModifiers.java
@@ -1,4 +1,15 @@
 package Chapter4;
 
 public class ApplyingAccessModifiers {
+    /**
+
+     Applying Access Modifiers
+     You already saw that there are four access modifiers: public, private, protected, and
+     default access. We are going to discuss them in order from most restrictive to least restrictive:
+     ■ private: Only accessible within the same class
+     ■ default (package private) access: private and other classes in the same package
+     ■ protected: default access and child classes
+     ■ public: protected and classes in the other packages
+
+     **/
 }
