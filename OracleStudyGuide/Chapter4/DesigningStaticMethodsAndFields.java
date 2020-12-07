666b6fc448961def099193ee2c0de1f6b976e53c
David-B-Comer <davec2233@gmail.com>
2020-12-07 08:59:33 -0500

    added designing static methods and fields own copy of code

--- OracleStudyGuide/Chapter4/DesigningStaticMethodsAndFields.java
+++ OracleStudyGuide/Chapter4/DesigningStaticMethodsAndFields.java
@@ -9,5 +9,17 @@ public class DesigningStaticMethodsAndFields {
      think of statics as being a member of the single class object that exist independently of any
      instances of that class.
 
+
+
+     Does Each Class Have Its Own Copy of the Code?
+     Each class has a copy of the instance variables. There is only one copy of the code for the
+     instance methods. Each instance of the class can call it as many times as it would like.
+     However, each call of an instance method (or any method) gets space on the stack for
+     method parameters and local variables.
+     The same thing happens for static methods. There is one copy of the code. Parameters
+     and local variables go on the stack.
+     Just remember that only data gets its “own copy.” There is no need to duplicate copies of
+     the code itself.
+
      **/
 }
