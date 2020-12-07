c40f01f33204c00cb72bd53f4205e4c680e5ce21
David-B-Comer <davec2233@gmail.com>
2020-12-07 08:58:31 -0500

    added designing static methods and fields explanation

--- OracleStudyGuide/Chapter4/DesigningStaticMethodsAndFields.java
+++ OracleStudyGuide/Chapter4/DesigningStaticMethodsAndFields.java
@@ -1,4 +1,13 @@
 package Chapter4;
 
 public class DesigningStaticMethodsAndFields {
+    /**
+
+     Designing Static Methods and Fields
+     Except for the main() method, we’ve been looking at instance methods. Static methods
+     don’t require an instance of the class. They are shared among all users of the class. You can
+     think of statics as being a member of the single class object that exist independently of any
+     instances of that class.
+
+     **/
 }
