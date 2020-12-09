c5cff0ab91ad03041b280137ac9d8f356ddfce0b
David-B-Comer <davec2233@gmail.com>
2020-12-09 08:50:17 -0500

    added static variables final constants

--- OracleStudyGuide/Chapter4/DesigningStaticMethodsAndFields.java
+++ OracleStudyGuide/Chapter4/DesigningStaticMethodsAndFields.java
@@ -87,5 +87,17 @@ public class DesigningStaticMethodsAndFields {
      private static int counter = 0; // initialization
      }
 
+
+
+     Other static variables are meant to never change during the program. This type of variable is known as a constant. It uses the final modifier to ensure the variable never changes.
+     static final constants use a different naming convention than other variables. They use
+     all uppercase letters with underscores between “words.” For example:
+     public class Initializers {
+     private static final int NUM_BUCKETS = 45;
+     public static void main(String[] args) {
+     NUM_BUCKETS = 5; // DOES NOT COMPILE
+     } }
+     The compiler will make sure that you do not accidentally try to update a final variable.
+
      **/
 }
