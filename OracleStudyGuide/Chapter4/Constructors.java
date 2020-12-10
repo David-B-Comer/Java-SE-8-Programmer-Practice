d4f0a5e067993ac32d5eec3f8ba30fff4ea55a98
David-B-Comer <davec2233@gmail.com>
2020-12-10 10:27:38 -0500

    added final fields

--- OracleStudyGuide/Chapter4/Constructors.java
+++ OracleStudyGuide/Chapter4/Constructors.java
@@ -70,5 +70,21 @@ public class Constructors {
      constructor add one parameter until getting to the constructor that does all the work.
      This approach is called constructor chaining.
 
+
+
+     Final Fields
+     As you saw earlier in the chapter, final instance variables must be assigned a value exactly
+     once. We saw this happen in the line of the declaration and in an instance initializer. There
+     is one more location this assignment can be done: in the constructor.
+     public class MouseHouse {
+     private final int volume;
+     private final String name = "The Mouse House";
+     public MouseHouse(int length, int width, int height) {
+     volume = length * width * height;
+     }}
+     The constructor is part of the initialization process, so it is allowed to assign final
+     instance variables in it. By  the time the constructor completes, all final instance variables
+     must have been set.
+
      **/
 }
