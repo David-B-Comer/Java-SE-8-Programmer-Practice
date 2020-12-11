6831041f0c92742154812f9c191f7dd110036427
David-B-Comer <davec2233@gmail.com>
2020-12-11 08:01:12 -0500

    added lambdas

---
+++ OracleStudyGuide/Chapter4/Lambdas.java
@@ -0,0 +1,31 @@
+package Chapter4;
+
+public class Lambdas {
+    /**
+
+
+     Writing Simple Lambdas
+     Java is an object-oriented language at heart. You’ve seen plenty of objects by now. In Java
+     8, the language added the ability to write code using another style. Functional programming is a way of
+     writing code more declaratively. You specify what you want to do rather
+     than dealing with the state of objects. You focus more on expressions than loops.
+     Functional programming uses lambda expressions to write code. A lambda expression
+     is a block of code that gets passed around. You can think of a lambda expression as an
+     anonymous method. It has parameters and a body just like full-fl edged methods do, but it
+     doesn’t have a name like a real method. Lambda expressions are often referred to as lambdas for short.
+     You might also know them as closures if Java isn’t your fi rst language. If you
+     had a bad experience with closures in the past, don’t worry. They are far simpler in Java.
+     In other words, a lambda expression is like a method that you can pass as if it were a
+     variable. For example, there are different ways to calculate age. One human year is equivalent to seven dog years.
+     You want to write a method that that takes an age() method as
+     input. To do this in an object-oriented program, you’d need to defi ne a Human subclass and a
+     Dog subclass. With lambdas, you can just pass in the relevant expression to calculate age.
+     Lambdas allow you to write powerful code in Java. Only the simplest lambda expressions are on the OCA exam.
+     The goal is to get you comfortable with the syntax and the
+     concepts. This means you aren’t truly doing functional programming yet. You’ll see
+     lambdas again on the OCP exam.
+     In this section, we’ll cover an example of why lambdas are helpful, the syntax of
+     lambdas, and how to use predicates.
+
+     **/
+}
