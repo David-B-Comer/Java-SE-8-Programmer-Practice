4a0098bb5eec61105e4a540466523ddb80ac73cb
David-B-Comer <davec2233@gmail.com>
2020-12-12 08:35:09 -0500

    added lambda syntax issues

--- OracleStudyGuide/Chapter4/Lambdas.java
+++ OracleStudyGuide/Chapter4/Lambdas.java
@@ -165,5 +165,16 @@ public class Lambdas {
      second line is missing the return keyword. The last line is missing the semicolon.
      You might have noticed all of our lambdas return a boolean. That is because the scope
      for the OCA exam limits what you need to learn.
+
+
+
+     There is one more issue you might see with lambdas. We’ve been defi ning an argument
+     list in our lambda expressions. Since Java doesn’t allow us to redeclare a local variable, the
+     following is an issue:
+     (a, b) -> { int a = 0; return 5;} // DOES NOT COMPILE
+     We tried to redeclare a, which is not allowed. By contrast, the following line is okay
+     because it uses a different variable name:
+     (a, b) -> { int c = 0; return 5;}
+
      **/
 }
