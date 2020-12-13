012882796872daeb63e625372401719733d52757
David-B-Comer <davec2233@gmail.com>
2020-12-13 18:02:07 -0500

    added chapter 4 review question 16

--- OracleStudyGuide/Chapter4/Chapter4ReviewQuestions.java
+++ OracleStudyGuide/Chapter4/Chapter4ReviewQuestions.java
@@ -383,5 +383,29 @@ public class Chapter4ReviewQuestions {
 
      Answer E
 
+
+
+
+
+     16. What is the result of the following program?
+
+     1: public class Squares {
+     2: public static long square(int x) {
+     3: long y = x * (long) x;
+     4: x = -1;
+     5: return y;
+     6: }
+     7: public static void main(String[] args) {
+     8: int value = 9;
+     9: long result = square(value);
+     10: System.out.println(value);
+     11: } }
+
+     A. -1
+     B. 9
+     C. 81
+     D. Compiler error on line 9.
+     E. Compiler error on a different line.
+
      **/
 }
