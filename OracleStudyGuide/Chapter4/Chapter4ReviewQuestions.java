6f10d6a74f0975914c8361e7ddc6d308ce04dbc8
David-B-Comer <davec2233@gmail.com>
2020-12-13 15:02:04 -0500

    added chapter 4 review question 9

--- OracleStudyGuide/Chapter4/Chapter4ReviewQuestions.java
+++ OracleStudyGuide/Chapter4/Chapter4ReviewQuestions.java
@@ -162,5 +162,18 @@ public class Chapter4ReviewQuestions {
 
      Answer B, C, E
 
+
+
+
+
+     9. Which are methods using JavaBeans naming conventions for accessors and mutators?
+     (Choose all that apply)
+
+     A. public boolean getCanSwim() { return canSwim;}
+     B. public boolean canSwim() { return numberWings;}
+     C. public int getNumWings() { return numberWings;}
+     D. public int numWings() { return numberWings;}
+     E. public void setCanSwim(boolean b) { canSwim = b;}
+
      **/
 }
