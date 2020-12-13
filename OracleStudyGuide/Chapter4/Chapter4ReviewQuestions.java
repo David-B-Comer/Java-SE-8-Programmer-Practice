4909f681d536df919d0b89dec3c5b58f422659a2
David-B-Comer <davec2233@gmail.com>
2020-12-13 13:54:36 -0500

    added chapter 4 review question 1

--- OracleStudyGuide/Chapter4/Chapter4ReviewQuestions.java
+++ OracleStudyGuide/Chapter4/Chapter4ReviewQuestions.java
@@ -1,4 +1,21 @@
 package Chapter4;
 
 public class Chapter4ReviewQuestions {
+    /**
+
+     1. Which of the following can fill in the blank in this code to make it compile? (Choose all
+     that apply)
+
+     public class Ant {
+     _____ void method() { }
+     }
+
+     A. default
+     B. final
+     C. private
+     D. Public
+     E. String
+     F. zzz:
+
+     **/
 }
