02682250ef05f76e6a615f8ebccb1bcc59f05e6d
David-B-Comer <davec2233@gmail.com>
2020-12-13 17:49:09 -0500

    added chapter 4 review question 14

--- OracleStudyGuide/Chapter4/Chapter4ReviewQuestions.java
+++ OracleStudyGuide/Chapter4/Chapter4ReviewQuestions.java
@@ -320,5 +320,28 @@ public class Chapter4ReviewQuestions {
 
      Answer E
 
+
+
+
+
+
+     14. Which of the following can replace line 2 to make this code compile? (Choose
+     all that apply)
+
+     1: import java.util.*;
+     2: // INSERT CODE HERE
+     3: public class Imports {
+     4: public void method(ArrayList<String> list) {
+     5: sort(list);
+     6: }
+     7: }
+
+     A. import static java.util.Collections;
+     B. import static java.util.Collections.*;
+     C. import static java.util.Collections.sort(ArrayList<String>);
+     D. static import java.util.Collections;
+     E. static import java.util.Collections.*;
+     F. static import java.util.Collections.sort(ArrayList<String>);
+
      **/
 }
