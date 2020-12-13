9590a1fbfc911c575181dddcdb419a8829e0aa34
David-B-Comer <davec2233@gmail.com>
2020-12-13 17:42:52 -0500

    added chapter 4 review question 13

--- OracleStudyGuide/Chapter4/Chapter4ReviewQuestions.java
+++ OracleStudyGuide/Chapter4/Chapter4ReviewQuestions.java
@@ -285,5 +285,37 @@ public class Chapter4ReviewQuestions {
 
      Answer D
 
+
+
+
+
+
+     13. How many compiler errors are in the following code?
+
+     1: public class RopeSwing {
+     2: private static final String leftRope;
+     3: private static final String rightRope;
+     4: private static final String bench;
+     5: private static final String name = "name";
+     6: static {
+     7: leftRope = "left";
+     8: rightRope = "right";
+     9: }
+     10: static {
+     11: name = "name";
+     12: rightRope = "right";
+     13: }
+     14: public static void main(String[] args) {
+     15: bench = "bench";
+     16: }
+     17: }
+
+     A. 0
+     B. 1
+     C. 2
+     D. 3
+     E. 4
+     F. 5
+
      **/
 }
