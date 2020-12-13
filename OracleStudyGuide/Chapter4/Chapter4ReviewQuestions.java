21d3406ab6feaabe5c3ef2cd3d0d0dc04a93619f
David-B-Comer <davec2233@gmail.com>
2020-12-13 17:20:17 -0500

    added chapter 4 review question 10

--- OracleStudyGuide/Chapter4/Chapter4ReviewQuestions.java
+++ OracleStudyGuide/Chapter4/Chapter4ReviewQuestions.java
@@ -178,5 +178,38 @@ public class Chapter4ReviewQuestions {
 
      Answer C, E
 
+
+
+
+     10. What is the output of the following code?
+
+     1: package rope;
+     2: public class Rope {
+     3: public static int LENGTH = 5;
+     4: static {
+     5: LENGTH = 10;
+     6: }
+     7: public static void swing() {
+     8: System.out.print("swing ");
+     9: }
+     10: }
+
+     1: import rope.*;
+     2: import static rope.Rope.*;
+     3: public class Chimp {
+     4: public static void main(String[] args) {
+     5: Rope.swing();
+     6: new Rope().swing();
+     7: System.out.println(LENGTH);
+     8: }
+     9: }
+
+     A. swing swing 5
+     B. swing swing 10
+     C. Compiler error on line 2 of Chimp.
+     D. Compiler error on line 5 of Chimp.
+     E. Compiler error on line 6 of Chimp.
+     F. Compiler error on line 7 of Chimp.
+
      **/
 }
