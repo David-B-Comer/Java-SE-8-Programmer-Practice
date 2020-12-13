9d82d343bf339c17c9160d8f410bed0ac1db0be3
David-B-Comer <davec2233@gmail.com>
2020-12-13 14:39:39 -0500

    added chapter 4 review question 7

--- OracleStudyGuide/Chapter4/Chapter4ReviewQuestions.java
+++ OracleStudyGuide/Chapter4/Chapter4ReviewQuestions.java
@@ -109,5 +109,39 @@ public class Chapter4ReviewQuestions {
 
      Answer D
 
+
+
+
+
+     7. Given the following my.school.ClassRoom and my.city.School class definitions, which
+     line numbers in main() generate a compiler error? (Choose all that apply)
+
+     1: package my.school;
+     2: public class Classroom {
+     3: private int roomNumber;
+     4: protected String teacherName;
+     5: static int globalKey = 54321;
+     6: public int floor = 3;
+     7: Classroom(int r, String t) {
+     8: roomNumber = r;
+     9: teacherName = t; } }
+
+     1: package my.city;
+     2: import my.school.*;
+     3: public class School {
+     4: public static void main(String[] args) {
+     5: System.out.println(Classroom.globalKey);
+     6: Classroom room = new Classroom(101, ""Mrs. Anderson");
+     7: System.out.println(room.roomNumber);
+     8: System.out.println(room.floor);
+     9: System.out.println(room.teacherName); } }
+
+     A. None, the code compiles fine.
+     B. Line 5
+     C. Line 6
+     D. Line 7
+     E. Line 8
+     F. Line 9
+
      **/
 }
