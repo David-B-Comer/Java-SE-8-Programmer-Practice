ffc2aabbf620f97f559d813592b1adda7d5532e1
David-B-Comer <davec2233@gmail.com>
2020-12-13 13:58:11 -0500

    added chapter 4 review question 2

--- OracleStudyGuide/Chapter4/Chapter4ReviewQuestions.java
+++ OracleStudyGuide/Chapter4/Chapter4ReviewQuestions.java
@@ -20,5 +20,18 @@ public class Chapter4ReviewQuestions {
 
      Answer B, C
 
+
+
+
+
+     2. Which of the following compile? (Choose all that apply)
+
+     A. final static void method4() { }
+     B. public final int void method() { }
+     C. private void int method() { }
+     D. static final void method3() { }
+     E. void final method() {}
+     F. void public method() { }
+
      **/
 }
