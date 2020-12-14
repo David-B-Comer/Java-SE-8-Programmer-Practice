9bd2c512100dceca58b65beb5a948175549a9267
David-B-Comer <davec2233@gmail.com>
2020-12-14 15:39:40 -0500

    added chapter 4 review question 18

--- OracleStudyGuide/Chapter4/Chapter4ReviewQuestions.java
+++ OracleStudyGuide/Chapter4/Chapter4ReviewQuestions.java
@@ -445,5 +445,20 @@ public class Chapter4ReviewQuestions {
 
      Answer B, D, E
 
+
+
+
+
+
+     18. Which of the following are true? (Choose 2)
+
+     A. this() can be called from anywhere in a constructor.
+     B. this() can be called from any instance method in the class.
+     C. this.variableName can be called from any instance method in the class.
+     D. this.variableName can be called from any static method in the class.
+     E. You must include a default constructor in the code if the compiler does not include one.
+     F. You can call the default constructor written by the compiler using this().
+     G. You can access a private constructor with the main() method.
+
      **/
 }
