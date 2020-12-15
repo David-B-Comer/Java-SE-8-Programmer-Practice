e432495e9c6614ce18b88d1a5dcdb81891aabb0d
David-B-Comer <davec2233@gmail.com>
2020-12-14 22:53:22 -0500

    added chapter 4 review question 21

--- OracleStudyGuide/Chapter4/Chapter4ReviewQuestions.java
+++ OracleStudyGuide/Chapter4/Chapter4ReviewQuestions.java
@@ -513,5 +513,30 @@ public class Chapter4ReviewQuestions {
 
      Answer E
 
+
+
+
+
+
+     21. Which of the following complete the constructor so that this code prints out 50? (Choose
+     all that apply)
+
+     public class Cheetah {
+     int numSpots;
+     public Cheetah(int numSpots) {
+     // INSERT CODE HERE
+     }
+     public static void main(String[] args) {
+     System.out.println(new Cheetah(50).numSpots);
+     }
+     }
+
+     A. numSpots = numSpots;
+     B. numSpots = this.numSpots;
+     C. this.numSpots = numSpots;
+     D. numSpots = super.numSpots;
+     E. super.numSpots = numSpots;
+     F. None of the above.
+
      **/
 }
