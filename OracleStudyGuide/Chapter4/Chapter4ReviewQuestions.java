bac0eed8e21ef041b8471c3b4f2574e92a735053
David-B-Comer <davec2233@gmail.com>
2020-12-14 23:41:18 -0500

    added chapter 4 review question 29

--- OracleStudyGuide/Chapter4/Chapter4ReviewQuestions.java
+++ OracleStudyGuide/Chapter4/Chapter4ReviewQuestions.java
@@ -762,5 +762,30 @@ public class Chapter4ReviewQuestions {
 
      Answer A, D, F
 
+
+
+
+
+
+
+     29. Which lambda can replace the MySecret class to return the same value? (Choose
+     all that apply)
+
+     interface Secret {
+     String magic(double d);
+     }
+     class MySecret implements Secret {
+     public String magic(double d) {
+     return "Poof";
+     }
+     }
+
+     A. caller((e) -> "Poof");
+     B. caller((e) -> {"Poof"});
+     C. caller((e) -> { String e = ""; "Poof" });
+     D. caller((e) -> { String e = ""; return "Poof"; });
+     E. caller((e) -> { String e = ""; return "Poof" });
+     F. caller((e) -> { String f = ""; return "Poof"; });
+
      **/
 }
