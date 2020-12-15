4be3b778e21f2c471cd85ab6fa27cc9eef943a74
David-B-Comer <davec2233@gmail.com>
2020-12-15 16:36:21 -0500

    added class design

---
+++ OracleStudyGuide/Chapter5/ClassDesign.java
@@ -0,0 +1,14 @@
+package Chapter5;
+
+public class ClassDesign {
+    /**
+
+     At its core, proper Java class design is about code reusability, increased functionality,
+     and standardization. For example, by creating a new class that extends an existing class,
+     you may gain access to a slew of inherited primitives, objects, and methods. Alternatively,
+     by designing a standard interface for your application, you ensure that any class that implements
+     the interface has certain required methods defi ned. Finally, by creating abstract class
+     definitions, you’re defining a platform that other developers can extend and build on top of.
+
+     **/
+}
