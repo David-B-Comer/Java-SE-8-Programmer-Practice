2512ca8463c2462157e6434d9d513cfe44100336
David-B-Comer <davec2233@gmail.com>
2020-12-14 23:16:30 -0500

    added chapter 4 review question 24

--- OracleStudyGuide/Chapter4/Chapter4ReviewQuestions.java
+++ OracleStudyGuide/Chapter4/Chapter4ReviewQuestions.java
@@ -611,5 +611,31 @@ public class Chapter4ReviewQuestions {
 
      Answer A
 
+
+
+
+
+     24. Which of the following will compile when inserted in the following code? (Choose
+     all that apply)
+
+     public class Order3 {
+     final String value1 = "1";
+     static String value2 = "2";
+     String value3 = "3";
+     {
+     // CODE SNIPPET 1
+     }
+     static {
+     // CODE SNIPPET 2
+     }
+     }
+
+     A. value1 = "d"; instead of // CODE SNIPPET 1
+     B. value2 = "e"; instead of // CODE SNIPPET 1
+     C. value3 = "f"; instead of // CODE SNIPPET 1
+     D. value1 = "g"; instead of // CODE SNIPPET 2
+     E. value2 = "h"; instead of // CODE SNIPPET 2
+     F. value3 = "i"; instead of // CODE SNIPPET 2
+
      **/
 }
