35f3d955d5bc8f3b7a25163ed8674fe9e9f4d13f
David-B-Comer <davec2233@gmail.com>
2020-12-14 22:44:02 -0500

    added chapter 4 review question 19

--- OracleStudyGuide/Chapter4/Chapter4ReviewQuestions.java
+++ OracleStudyGuide/Chapter4/Chapter4ReviewQuestions.java
@@ -463,5 +463,19 @@ public class Chapter4ReviewQuestions {
 
      Answer C, G
 
+
+
+
+
+     19. Which of these classes compile and use a default constructor? (Choose all that apply)
+
+     A. public class Bird { }
+     B. public class Bird { public bird() {} }
+     C. public class Bird { public bird(String name) {} }
+     D. public class Bird { public Bird() {} }
+     E. public class Bird { Bird(String name) {} }
+     F. public class Bird { private Bird(int age) {} }
+     G. public class Bird { void Bird() { }
+
      **/
 }
