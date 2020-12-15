966baeb7aaa7d5caa1e355fbcfe8cc4cb7e23f00
David-B-Comer <davec2233@gmail.com>
2020-12-14 23:34:18 -0500

    added chapter 4 review question 27

--- OracleStudyGuide/Chapter4/Chapter4ReviewQuestions.java
+++ OracleStudyGuide/Chapter4/Chapter4ReviewQuestions.java
@@ -709,5 +709,36 @@ public class Chapter4ReviewQuestions {
 
      Answer A
 
+
+
+
+
+
+
+     27. What is the result of the following code?
+
+     1: interface Climb {
+     2: boolean isTooHigh(int height, int limit);
+     3: }
+     4:
+     5: public class Climber {
+     6: public static void main(String[] args) {
+     7: check((h, l) -> h.append(l).isEmpty(), 5);
+     8: }
+     9: private static void check(Climb climb, int height) {
+     10: if (climb.isTooHigh(height, 10))
+     11: System.out.println("too high");
+     12: else
+     13: System.out.println("ok");
+     14: }
+     15: }
+
+     A. ok
+     B. too high
+     C. Compiler error on line 7.
+     D. Compiler error on line 10.
+     E. Compiler error on a different line.
+     F. A runtime exception is thrown.
+
      **/
 }
