4cf384f2b698f2bce5e1dbcf9732c40e2363fba4
David-B-Comer <davec2233@gmail.com>
2020-12-14 23:10:31 -0500

    added chapter 4 review question 23

--- OracleStudyGuide/Chapter4/Chapter4ReviewQuestions.java
+++ OracleStudyGuide/Chapter4/Chapter4ReviewQuestions.java
@@ -577,5 +577,36 @@ public class Chapter4ReviewQuestions {
 
      Answer E
 
+
+
+
+
+
+     23. What is the result of the following?
+
+     1: public class Order {
+     2: String value = "t";
+     3: { value += "a"; }
+     4: { value += "c"; }
+     5: public Order() {
+     6: value += "b";
+     7: }
+     8: public Order(String s) {
+     9: value += s;
+     10: }
+     11: public static void main(String[] args) {
+     12: Order order = new Order("f");
+     13: order = new Order();
+     14: System.out.println(order.value);
+     15: } }
+
+     A. tacb
+     B. tacf
+     C. tacbf
+     D. tacfb
+     E. tacftacb
+     F. The code does not compile.
+     G. An exception is thrown.
+
      **/
 }
