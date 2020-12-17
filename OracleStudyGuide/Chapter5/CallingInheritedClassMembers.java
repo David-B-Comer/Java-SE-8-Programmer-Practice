b141fe339c02ea391aa1bab056e8e2ed9110fa4d
David-B-Comer <davec2233@gmail.com>
2020-12-16 19:29:39 -0500

    added calling inherited class members

---
+++ OracleStudyGuide/Chapter5/CallingInheritedClassMembers.java
@@ -0,0 +1,15 @@
+package Chapter5;
+
+public class CallingInheritedClassMembers {
+    /**
+
+     Calling Inherited Class Members
+     Java classes may use any public or protected member of the parent class, including methods, primitives, or object references. If the parent class and child class are part of the same
+     package, the child class may also use any default members defi ned in the parent class.
+     Finally, a child class may never access a private member of the parent class, at least not
+     through any direct reference. As you saw in the fi rst example in this chapter, a private
+     member age may be accessed indirectly via a public or protected method.
+     To reference a member in a parent class, you can just call it directly
+
+     **/
+}
