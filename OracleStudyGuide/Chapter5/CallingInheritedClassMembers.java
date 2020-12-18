482c4bf5f074976614d0c205823cf376c0130b02
David-B-Comer <davec2233@gmail.com>
2020-12-18 18:46:50 -0500

    added abstract class definition rules

--- OracleStudyGuide/Chapter5/CallingInheritedClassMembers.java
+++ OracleStudyGuide/Chapter5/CallingInheritedClassMembers.java
@@ -273,5 +273,19 @@ public class CallingInheritedClassMembers {
      as an abstract one. In other words, the subclasses do not consider it an inherited abstract
      method because it is no longer abstract by the time it reaches the subclasses.
 
+
+
+
+     The following are lists of rules for abstract classes and abstract methods that we have
+     covered in this section. Review and understand these rules before taking the exam.
+     Abstract Class Definition Rules:
+     1. Abstract classes cannot be instantiated directly.
+     2. Abstract classes may be defined with any number, including zero, of abstract and non-abstract methods.
+     3. Abstract classes may not be marked as private or final.
+     4. An abstract class that extends another abstract class inherits all of its abstract methods
+     as its own abstract methods.
+     5. The first concrete class that extends an abstract class must provide an implementation
+     for all of the inherited abstract methods.
+
      **/
 }
