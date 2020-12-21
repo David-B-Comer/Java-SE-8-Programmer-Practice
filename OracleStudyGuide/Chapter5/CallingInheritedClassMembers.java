366b515cb3963a09b63387bff2e0de30286a1b49
David-B-Comer <davec2233@gmail.com>
2020-12-21 18:41:43 -0500

    added polymorphic parameters and code reusability

--- OracleStudyGuide/Chapter5/CallingInheritedClassMembers.java
+++ OracleStudyGuide/Chapter5/CallingInheritedClassMembers.java
@@ -504,5 +504,21 @@ public class CallingInheritedClassMembers {
      explicit cast is not required. This property is referred to as polymorphic parameters of a
      method
 
+
+
+
+     Polymorphic Parameters and Code Reusability
+     If you’re defining a method that will be accessible outside the current class, either to
+     subclasses of the current class or publicly to objects outside the current class, it is considered
+     good coding practice to use the superclass or interface type of input parameters whenever possible.
+     As you may remember from Chapter 3, “Core Java APIs,” the type java.util.List is an
+     interface, not a class. Although there are many classes that implement java.util.List,
+     such as java.util.ArrayList and java.util.Vector, when you’re passing an existing
+     List you’re not usually interested in the particular subclass of the List. In this manner, a
+     method that passes a List should use the interface type java.util.List as the polymorphic parameter
+     type, rather than a specific class that implements List, as the code will be more reusable for other
+     types of lists. For example, it is common to see code such as the following that uses the interface
+     reference type over the class type for greater reusability: java.util.List list = new java.util.ArrayList();
+
      **/
 }
