f877c9b44de0bf3c86bc26b1d9f33d5e6048f82a
David-B-Comer <davec2233@gmail.com>
2020-12-22 14:32:33 -0500

    added chapter 5 review question 3

--- OracleStudyGuide/Chapter5/Chapter5ReviewQuestions.java
+++ OracleStudyGuide/Chapter5/Chapter5ReviewQuestions.java
@@ -46,5 +46,34 @@ public class Chapter5ReviewQuestions {
 
     Answer E
 
+
+
+
+
+
+
+    3. Which of the following statements can be inserted in the blank line so that the code will
+    compile successfully? (Choose all that apply)
+
+    public interface CanHop {}
+    public class Frog implements CanHop {
+    public static void main(String[] args) {
+    frog = new TurtleFrog();
+    }
+    }
+    public class BrazilianHornedFrog extends Frog {}
+    public class TurtleFrog extends Frog {}
+
+    A. Frog
+    B. TurtleFrog
+    C. BrazilianHornedFrog
+    D. CanHop
+    E. Object
+    F. Long
+
+
+
+
+
  **/
 }
