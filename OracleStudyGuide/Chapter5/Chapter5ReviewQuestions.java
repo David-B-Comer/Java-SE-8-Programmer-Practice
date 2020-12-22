c8c8563987599f0a42d26e701deeb3b8b2e8813b
David-B-Comer <davec2233@gmail.com>
2020-12-22 14:25:42 -0500

    added chapter 5 review question 1 answer

--- OracleStudyGuide/Chapter5/Chapter5ReviewQuestions.java
+++ OracleStudyGuide/Chapter5/Chapter5ReviewQuestions.java
@@ -3,12 +3,16 @@ package Chapter5;
 public class Chapter5ReviewQuestions {
 /**
     1. What modifiers are implicitly applied to all interface methods? (Choose all that apply)
+
     A. protected
     B. public
     C. static
     D. void
     E. abstract
     F. default
+
+
+ Answer B
 
  **/
 }
