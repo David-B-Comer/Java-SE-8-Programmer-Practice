0d3b708fb05294f01ed8efca113eea6f64e623f8
David-B-Comer <davec2233@gmail.com>
2020-12-22 14:27:35 -0500

    added chapter 5 review question 2

--- OracleStudyGuide/Chapter5/Chapter5ReviewQuestions.java
+++ OracleStudyGuide/Chapter5/Chapter5ReviewQuestions.java
@@ -12,7 +12,36 @@ public class Chapter5ReviewQuestions {
     F. default
 
 
- Answer B
+    Answer B
+
+
+
+
+
+    2. What is the output of the following code?
+
+    1: class Mammal {
+    2: public Mammal(int age) {
+    3: System.out.print("Mammal");
+    4: }
+    5: }
+    6: public class Platypus extends Mammal {
+    7: public Platypus() {
+    8: System.out.print("Platypus");
+    9: }
+    10: public static void main(String[] args) {
+    11: new Mammal(5);
+    12: }
+    13: }
+
+    A. Platypus
+    B. Mammal
+    C. PlatypusMammal
+    D. MammalPlatypus
+    E. The code will not compile because of line 8.
+    F. The code will not compile because of line 11.
+
+
 
  **/
 }
