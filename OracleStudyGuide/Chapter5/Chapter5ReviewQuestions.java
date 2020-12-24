4aa20a6283e4620afa0b96175967d8b01f8b8477
David-B-Comer <davec2233@gmail.com>
2020-12-23 19:30:55 -0500

    added chapter 5 review question 9

--- OracleStudyGuide/Chapter5/Chapter5ReviewQuestions.java
+++ OracleStudyGuide/Chapter5/Chapter5ReviewQuestions.java
@@ -189,5 +189,27 @@ public class Chapter5ReviewQuestions {
 
     Answer F
 
+
+
+
+
+    9. Choose the correct statement about the following code:
+
+    1: public interface CanFly {
+    2: void fly();
+    3: }
+    4: interface HasWings {
+    5: public abstract Object getWindSpan();
+    6: }
+    7: abstract class Falcon implements CanFly, HasWings {
+    8: }
+
+    A. It compiles without issue.
+    B. The code will not compile because of line 2.
+    C. The code will not compile because of line 4.
+    D. The code will not compile because of line 5.
+    E. The code will not compile because of lines 2 and 5.
+    F. The code will not compile because the class Falcon doesn’t implement the interface methods.
+
  **/
 }
