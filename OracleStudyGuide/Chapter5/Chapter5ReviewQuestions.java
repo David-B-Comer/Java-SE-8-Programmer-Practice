1dd38ae79626f0e982bdd47ad6796c44c5c5a711
David-B-Comer <davec2233@gmail.com>
2020-12-25 20:23:03 -0500

    added chapter 5 review question 16

--- OracleStudyGuide/Chapter5/Chapter5ReviewQuestions.java
+++ OracleStudyGuide/Chapter5/Chapter5ReviewQuestions.java
@@ -336,5 +336,31 @@ public class Chapter5ReviewQuestions {
 
     Answer B
 
+
+
+
+
+
+
+    16. What is the output of the following code?
+
+    1: abstract class Reptile {
+    2: public final void layEggs() { System.out.println("Reptile laying eggs");
+    }
+    3: public static void main(String[] args) {
+    4: Reptile reptile = new Lizard();
+    5: reptile.layEggs();
+    6: }
+    7: }
+    8: public class Lizard extends Reptile {
+    9: public void layEggs() { System.out.println("Lizard laying eggs"); }
+    10: }
+
+    A. Reptile laying eggs
+    B. Lizard laying eggs
+    C. The code will not compile because of line 4.
+    D. The code will not compile because of line 5.
+    E. The code will not compile because of line 9.
+
  **/
 }
