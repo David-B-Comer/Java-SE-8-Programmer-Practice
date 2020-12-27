480dd0a65fb129c3b52371fb73611fb23b94bb27
David-B-Comer <davec2233@gmail.com>
2020-12-27 18:04:56 -0500

    added understanding exceptions

---
+++ OracleStudyGuide/Chapter6/UnderstandingExceptions.java
@@ -0,0 +1,18 @@
+package Chapter6;
+
+public class UnderstandingExceptions {
+    /**
+
+     Understanding Exceptions
+     A program can fail for just about any reason. Here are just a few possibilities:
+     ■ The code tries to connect to a website, but the Internet connection is down.
+     ■ You made a coding mistake and tried to access an invalid index in an array.
+     ■ One method calls another with a value that the method doesn’t support.
+     As you can see, some of these are coding mistakes. Others are completely beyond your
+     control. Your program can’t help it if the Internet connection goes down. What it can do is
+     deal with the situation.
+     First, we’ll look at the role of exceptions. Then we’ll cover the various types of exceptions,
+     followed by an explanation of how to throw an exception in Java.
+
+     **/
+}
