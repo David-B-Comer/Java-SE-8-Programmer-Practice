eabdffaeaa7632d914c4dc555edac0046b79b4a1
David-B-Comer <davec2233@gmail.com>
2020-12-28 17:21:28 -0500

    added error explanation

--- OracleStudyGuide/Chapter6/UnderstandingExceptions.java
+++ OracleStudyGuide/Chapter6/UnderstandingExceptions.java
@@ -88,5 +88,20 @@ public class UnderstandingExceptions {
 
 
 
+
+
+     Error means something went so horribly wrong that your program should not attempt to
+     recover from it. For example, the disk drive “disappeared.” These are abnormal conditions
+     that you aren’t likely to encounter.
+     A runtime exception is defined as the RuntimeException class and its subclasses. Runtime
+     exceptions tend to be unexpected but not necessarily fatal. For example, accessing an invalid
+     array index is unexpected. Runtime exceptions are also known as unchecked exceptions.
+     A checked exception includes Exception and all subclasses that do not extend
+     RuntimeException. Checked exceptions tend to be more anticipated—for example, trying
+     to read a file that doesn’t exist.
+     Checked exceptions? What are we checking? Java has a rule called the handle or declare
+     rule. For checked exceptions, Java requires the code to either handle them or declare them
+     in the method signature.
+
      **/
 }
