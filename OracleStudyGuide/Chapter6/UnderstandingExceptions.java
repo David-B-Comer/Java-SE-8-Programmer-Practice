9eb2e5425bd1ccd93f897b6180ff0226264a6e7f
David-B-Comer <davec2233@gmail.com>
2020-12-31 18:12:34 -0500

    added checked exceptions

--- OracleStudyGuide/Chapter6/UnderstandingExceptions.java
+++ OracleStudyGuide/Chapter6/UnderstandingExceptions.java
@@ -412,5 +412,18 @@ public class UnderstandingExceptions {
 
 
 
+     Checked Exceptions
+
+     Checked exceptions have Exception in their hierarchy but not RuntimeException. They
+     must be handled or declared. They can be thrown by the programmer or by the JVM.
+     Common runtime exceptions include the following:
+     FileNotFoundException Thrown programmatically when code tries to reference a fi le
+     that does not exist
+     IOException Thrown programmatically when there’s a problem reading or writing a fi le
+     For the OCA exam, you only need to know that these are checked exceptions. Also keep
+     in mind that FileNotFoundException is a subclass of IOException, although the exam will
+     remind you of that fact if it comes up. You’ll see these two exceptions in more detail on the
+     OCP exam.
+
      **/
 }
