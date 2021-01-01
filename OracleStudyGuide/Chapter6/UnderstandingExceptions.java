ff8f53226720ab0dd1810e401efff67f13d20306
David-B-Comer <davec2233@gmail.com>
2021-01-01 10:42:24 -0500

    added exception in initializer error

--- OracleStudyGuide/Chapter6/UnderstandingExceptions.java
+++ OracleStudyGuide/Chapter6/UnderstandingExceptions.java
@@ -435,12 +435,41 @@ public class UnderstandingExceptions {
 
      Errors extend the Error class. They are thrown by the JVM and should not be handled or
      declared. Errors are rare, but you might see these:
+
      ExceptionInInitializerError Thrown by the JVM when a static initializer throws
      an exception and doesn’t handle it
+
      StackOverflowError Thrown by the JVM when a method calls itself too many times
      (this is called infinite recursion because the method typically calls itself without end)
+
      NoClassDefFoundError Thrown by the JVM when a class that the code uses is available
      at compile time but not runtime
+
+
+
+
+
+     ExceptionInInitializerError
+
+     Java runs static initializers the first time a class is used. If one of the static initializers
+     throws an exception, Java can’t start using the class. It declares defeat by throwing an
+     ExceptionInInitializerError. This code shows an ArrayIndexOutOfBounds in a static
+     initializer:
+     static {
+     int[] countsOfMoose = new int[3];
+     int num = countsOfMoose[-1];
+     }
+     public static void main(String[] args) { }
+     This code yields information about two exceptions:
+     Exception in thread "main" java.lang.ExceptionInInitializerError
+     Caused by: java.lang.ArrayIndexOutOfBoundsException: -1
+     We get the ExceptionInInitializerError because the error happened in a static initializer.
+     That information alone wouldn’t be particularly useful in fixing the problem. Therefore,
+     Java also tells us the original cause of the problem: the ArrayIndexOutOfBoundsException that
+     we need to fix.
+     The ExceptionInInitializerError is an error because Java failed to load the whole
+     class. This failure prevents Java from continuing.
+
 
      **/
 }
