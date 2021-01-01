ef285453ee0cef95f696c7629bcdcdb09a7d4975
David-B-Comer <davec2233@gmail.com>
2021-01-01 10:44:03 -0500

    added stack overflow error

--- OracleStudyGuide/Chapter6/UnderstandingExceptions.java
+++ OracleStudyGuide/Chapter6/UnderstandingExceptions.java
@@ -471,5 +471,23 @@ public class UnderstandingExceptions {
      class. This failure prevents Java from continuing.
 
 
+
+
+
+     StackOverflowError
+
+     When Java calls methods, it puts parameters and local variables on the stack. After doing
+     this a very large number of times, the stack runs out of room and overflows. This is called a
+     StackOverflowError. Most of the time, this error occurs when a method calls itself.
+     public static void doNotCodeThis(int num) {
+     doNotCodeThis(1);
+     }
+     The output contains this line:
+     Exception in thread "main" java.lang.StackOverflowError
+     Since the method calls itself, it will never end. Eventually, Java runs out of room on the
+     stack and throws the error. This is called infinite recursion. It is better than an infinite loop
+     because at least Java will catch it and throw the error. With an infinite loop, Java just uses
+     all your CPU until you can kill it.
+
      **/
 }
