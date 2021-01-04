6bf8d934584f8b0e9c466497396d0e727430c989
David-B-Comer <davec2233@gmail.com>
2021-01-03 19:22:45 -0500

    added chapter 6 review question 1

---
+++ OracleStudyGuide/Chapter6/Chapter6ReviewQuestions.java
@@ -0,0 +1,16 @@
+package Chapter6;
+
+public class Chapter6ReviewQuestions {
+    /**
+
+    1. Which of the following statements are true? (Choose all that apply)
+
+    A. Runtime exceptions are the same thing as checked exceptions.
+    B. Runtime exceptions are the same thing as unchecked exceptions.
+    C. You can declare only checked exceptions.
+    D. You can declare only unchecked exceptions.
+    E. You can handle only Exception subclasses.
+
+
+     **/
+}
