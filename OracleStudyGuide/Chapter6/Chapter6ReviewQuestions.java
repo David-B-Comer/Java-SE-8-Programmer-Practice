b2d90c54bcc080b4bce230d5c66d5c39e0dc9fe7
David-B-Comer <davec2233@gmail.com>
2021-01-03 19:30:27 -0500

    added chapter 6 review question 3

--- OracleStudyGuide/Chapter6/Chapter6ReviewQuestions.java
+++ OracleStudyGuide/Chapter6/Chapter6ReviewQuestions.java
@@ -34,5 +34,20 @@ public class Chapter6ReviewQuestions {
 
     Answer B, D
 
+
+
+
+
+
+     3. When are you required to use a finally block in a regular try statement (not a try-withresources)?
+
+     A. Never.
+     B. When the program code doesn’t terminate on its own.
+     C. When there are no catch blocks in a try statement.
+     D. When there is exactly one catch block in a try statement.
+     E. When there are two or more catch blocks in a try statement.
+
+
+
      **/
 }
