d9977c0af96c89403c4f070c78d1aa5798637989
David-B-Comer <davec2233@gmail.com>
2021-01-05 14:53:16 -0500

    added chapter 6 review question 7

--- OracleStudyGuide/Chapter6/Chapter6ReviewQuestions.java
+++ OracleStudyGuide/Chapter6/Chapter6ReviewQuestions.java
@@ -98,5 +98,40 @@ public class Chapter6ReviewQuestions {
 
      Answer C
 
+
+
+
+
+     7. What is printed besides the stack trace caused by the NullPointerException from line 16?
+
+     1: public class DoSomething {
+     2: public void go() {
+     3: System.out.print("A");
+     4: try {
+     5: stop();
+     6: } catch (ArithmeticException e) {
+     7: System.out.print("B");
+     8: } finally {
+     9: System.out.print("C");
+     10: }
+     11: System.out.print("D");
+     12: }
+     13: public void stop() {
+     14: System.out.print("E");
+     15: Object x = null;
+     16: x.toString();
+     17: System.out.print("F");
+     18: }
+     19: public static void main(String[] args) {
+     20: new DoSomething().go();
+     21: }
+     22: }
+
+     A. AE
+     B. AEBCD
+     C. AEC
+     D. AECD
+     E. No output appears other than the stack trace.
+
      **/
 }
