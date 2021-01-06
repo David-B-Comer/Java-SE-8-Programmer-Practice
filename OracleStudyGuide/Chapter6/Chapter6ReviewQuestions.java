dc41b9ec18ee7eb9e24c6d4b9b3bc6daa4a63cf6
David-B-Comer <davec2233@gmail.com>
2021-01-06 18:44:26 -0500

    added chapter 6 review question 11

--- OracleStudyGuide/Chapter6/Chapter6ReviewQuestions.java
+++ OracleStudyGuide/Chapter6/Chapter6ReviewQuestions.java
@@ -231,5 +231,39 @@ public class Chapter6ReviewQuestions {
 
      Answer E
 
+
+
+
+     11. What is the output of the following program?
+
+     1: public class Cat {
+     2: public String name;
+     3: public void parseName() {
+     4: System.out.print("1");
+     5: try {
+     6: System.out.print("2");
+     7: int x = Integer.parseInt(name);
+     8: System.out.print("3");
+     9: } catch (NullPointerException e) {
+     10: System.out.print("4");
+     11: }
+     12: System.out.print("5");
+     13: }
+     14: public static void main(String[] args) {
+     15: Cat leo = new Cat();
+     16: leo.name = "Leo";
+     17: leo.parseName();
+     18: System.out.print("6");
+     19: }
+     20: }
+
+     A. 12, followed by a stack trace for a NumberFormatException
+     B. 124, followed by a stack trace for a NumberFormatException
+     C. 12456
+     D. 12456
+     E. 1256, followed by a stack trace for a NumberFormatException
+     F. The code does not compile.
+     G. An uncaught exception is thrown.
+
      **/
 }
