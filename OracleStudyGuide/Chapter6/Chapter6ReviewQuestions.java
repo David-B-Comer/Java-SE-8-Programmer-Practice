da40bd65656c298ce260c54c15299f17b39ff196
David-B-Comer <davec2233@gmail.com>
2021-01-09 08:11:08 -0500

    added chapter 6 review question 20

--- OracleStudyGuide/Chapter6/Chapter6ReviewQuestions.java
+++ OracleStudyGuide/Chapter6/Chapter6ReviewQuestions.java
@@ -438,5 +438,36 @@ public class Chapter6ReviewQuestions {
 
      Answer C, E
 
+
+
+
+
+
+     20. What does the output of the following contain? (Choose all that apply)
+
+     12: public static void main(String[] args) {
+     13: System.out.print("a");
+     14: try {
+     15: System.out.print("b");
+     16: throw new IllegalArgumentException();
+     17: } catch (IllegalArgumentException e) {
+     18: System.out.print("c");
+     19: throw new RuntimeException("1");
+     20: } catch (RuntimeException e) {
+     21: System.out.print("d");
+     22: throw new RuntimeException("2");
+     23: } finally {
+     24: System.out.print("e");
+     25: throw new RuntimeException("3");
+     26: }
+     27: }
+
+     A. abce
+     B. abde
+     C. An exception with the message set to "1"
+     D. An exception with the message set to "2"
+     E. An exception with the message set to "3"
+     F. Nothing; the code does not compile.
+
      **/
 }
