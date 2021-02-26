eb468d3bbf016b0d305ec045530dd0257003c34b
David-B-Comer <davec2233@gmail.com>
2021-02-26 14:49:42 -0500

    added practice question 1

---
+++ OracleStudyGuide/PracticeQuestions/questions.java
@@ -0,0 +1,18 @@
+package PracticeQuestions;
+
+public class questions {
+
+    /**
+
+     1. Given the following:
+
+     What is the result?
+     65432
+     12345
+     Nothing
+     An exception could be thrown at runtime.
+     654321
+     123456
+
+     **/
+}
