060f6f0dab6c3582fca24d7b65a0d04bc990c16f
David-B-Comer <davec2233@gmail.com>
2021-03-04 19:54:55 -0500

    added sample question 1

---
+++ OracleStudyGuide/PracticeQuestions/SampleQuestions.java
@@ -0,0 +1,13 @@
+package PracticeQuestions;
+
+public class SampleQuestions {
+    /**
+
+     01. Which of the following is a valid JavaBean method signature?
+     a) public void getArrow()
+     b) public void setBow()
+     c) public void setRange(int range)
+     d) public String addTarget(String target)
+
+     **/
+}
