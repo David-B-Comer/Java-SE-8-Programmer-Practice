c9fa519930ecc7774b1ecf91c37724a62b11c249
David-B-Comer <davec2233@gmail.com>
2021-03-04 19:55:36 -0500

    added sample question 2

--- OracleStudyGuide/PracticeQuestions/SampleQuestions.java
+++ OracleStudyGuide/PracticeQuestions/SampleQuestions.java
@@ -9,5 +9,15 @@ public class SampleQuestions {
      c) public void setRange(int range)
      d) public String addTarget(String target)
 
+
+
+     02. Of the following four modifiers, choose the one that is not implicitly applied to all interface variables.
+     a) final
+     b) abstract
+     c) static
+     d) public
+
+
+
      **/
 }
