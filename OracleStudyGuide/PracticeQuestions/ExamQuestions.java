c0d053e9045776fe98526ba756cf12456959a1f2
David-B-Comer <davec2233@gmail.com>
2021-03-09 17:51:28 -0500

    added exam question 4

--- OracleStudyGuide/PracticeQuestions/ExamQuestions.java
+++ OracleStudyGuide/PracticeQuestions/ExamQuestions.java
@@ -81,5 +81,25 @@ public class ExamQuestions {
 
      ANSWER: D
 
+
+
+     Question # 4
+     Given the code fragment:
+
+     public static void main(String[] args {
+        short s1 = 200;
+        Integer s2 = 400;
+        Long s3 = (long) s1 + s2;           //line n1
+        String s4 = (String) (s3 * s2);     //line n2
+        System.out.println("Sum is " + s4);
+     }
+
+     What is the result?
+     A. Sum is 600
+     B. Compilation fails at line n1.
+     C. Compilation fails at line n2.
+     D. A ClassCastException is thrown at line n1.
+     E. A ClassCastException is thrown at line n2.
+
      **/
 }
