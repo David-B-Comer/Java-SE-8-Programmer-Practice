b8f02353eea8980a32373f4789cd13002246912d
David-B-Comer <davec2233@gmail.com>
2021-03-11 20:40:09 -0500

    added exam question 12

--- OracleStudyGuide/PracticeQuestions/ExamQuestions.java
+++ OracleStudyGuide/PracticeQuestions/ExamQuestions.java
@@ -372,5 +372,28 @@ public class ExamQuestions {
      ANSWER: C
 
 
+
+
+
+     Question #12
+     Given:
+
+     public static void main(String[] args) {
+        String ta = "A ";
+        ta = ta.concat("B ");
+        String tb = "C ";
+        ta = ta.concat(tb);
+        ta.replace('C'. 'D');
+        ta = ta.concat(tb);
+        System.out.println(ta);
+     }
+
+     What is the result?
+     A. A B C D
+     B. A C D
+     C. A B C C
+     D. A B D
+     E. A B D C
+
      **/
 }
