10944fa4ead606f38bcab07f0dbdabfe47c5d15f
David-B-Comer <davec2233@gmail.com>
2021-03-11 20:24:07 -0500

    added exam question 10

--- OracleStudyGuide/PracticeQuestions/ExamQuestions.java
+++ OracleStudyGuide/PracticeQuestions/ExamQuestions.java
@@ -305,5 +305,33 @@ public class ExamQuestions {
 
      ANSWER: C
 
+
+
+
+
+     Question #10
+     Given the code fragment:
+
+     public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder(5);
+        String s = "";
+
+        if (sb.equals(s)) {
+            System.out.println("Match 1");
+        } else if (sb.toString().equals(s.toString())) {
+            System.out.println("Match 2");
+        } else {
+            System.out.println("No Match");
+        }
+     }
+
+     What is the result?
+     A. Match 1
+     B. Match 2
+     C. No Match
+     D. A NullPointerException is thrown at runtime.
+
+
+
      **/
 }
