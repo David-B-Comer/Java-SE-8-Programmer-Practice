6c45fe4cbfb6c760a9895a2d2062e6191ee1563d
David-B-Comer <davec2233@gmail.com>
2021-03-11 20:33:49 -0500

    added exam question 11

--- OracleStudyGuide/PracticeQuestions/ExamQuestions.java
+++ OracleStudyGuide/PracticeQuestions/ExamQuestions.java
@@ -336,5 +336,37 @@ public class ExamQuestions {
 
      ANSWER: B
 
+
+
+
+
+     Question #11
+     Given:
+
+     interface Readable {
+        public void readBook();
+        public void setBookMark();
+     }
+     abstract class Book implements Readable {      // line n1
+        public void readBook() { }
+        // line n2
+     }
+
+     class EBook extends Book {                     // line n3
+        public void readBook() { }
+        // line n4
+     }
+     Which option enables the code to compile?
+
+     A) Replace the code fragment at line n1 with: class Book implements Readable {
+     B) At line n2 insert: public abstract void setBookMark();
+     C) Replace the code fragment at line n3 with: abstract class EBook extends Book {
+     D) At line n4 insert: public void setBookMark() { }
+
+     A. Option A
+     B. Option B
+     C. Option C
+     D. Option D
+
      **/
 }
