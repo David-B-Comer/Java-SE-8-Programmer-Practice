c084b10caddab05944a0f1dc58e2aaa827fbe008
David-B-Comer <davec2233@gmail.com>
2021-03-13 19:38:36 -0500

    added exam question 18

--- OracleStudyGuide/PracticeQuestions/ExamQuestions.java
+++ OracleStudyGuide/PracticeQuestions/ExamQuestions.java
@@ -567,5 +567,48 @@ public class ExamQuestions {
 
      ANSWER: D, G, H
 
+
+
+
+     Question #18
+     Given the code fragment:
+
+     String shirts[][] = new String[2][2];
+     shirts[0][0] = "red";
+     shirts[0][1] = "blue";
+     shirts[1][0] = "small";
+     shirts[1][1] = "medium";
+
+     Which code fragment prints red: blue: small: medium?
+
+     A) for (int index = 1; index < 2; index++) {
+            for (int idx = 1; idx < 2; idx++) {
+                System.out.print(shirts[index][idx] + ":");
+            }
+        }
+     B) for (int index = 0; index < 2; ++index) {
+            for (int idx = 0; idx < index; ++idx) {
+                System.out.print(shirts[index][idx] + ":");
+            }
+        }
+     C) for (String c : colors) {
+            for (String s : sizes) {
+                System.out.println(s + ":");
+            }
+        }
+     D) for (int index = 0; index < 2;) {
+            for (int idx = 0; idx < 2;) {
+                System.out.print(shirts[index][idx] + ":");
+                idx++;
+            }
+            index++;
+        }
+
+
+     A. Option A
+     B. Option B
+     C. Option C
+     D. Option D
+
      **/
 }
