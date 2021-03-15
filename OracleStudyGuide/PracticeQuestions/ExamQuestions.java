315c46b3bd1be76ddc5f671d4023cbcceb5f5809
David-B-Comer <davec2233@gmail.com>
2021-03-14 20:22:28 -0400

    added exam question 20

--- OracleStudyGuide/PracticeQuestions/ExamQuestions.java
+++ OracleStudyGuide/PracticeQuestions/ExamQuestions.java
@@ -648,5 +648,32 @@ public class ExamQuestions {
      ANSWER: D
 
 
+
+
+
+
+     Question #20
+     Given the code fragment:
+
+     3. public static void main(String[] args) {
+     4.     int x = 5;
+     5.     while (isAvailable(x)) {
+     6.         System.out.print(x);
+     7.
+     8.     }
+     9. }
+     10.
+     11. public static boolean isAvailable(int x) {
+     12.    return x-- > 0 ? true : false;
+     13. }
+
+
+     Which modification enables the code to print 54321?
+     A. Replace line 6 with System, out. print (--x) ;
+     B. At line 7, insert x --;
+     C. Replace line 6 with --x; and, at line 7, insert system, out. print (x);
+     D. Replace line 12 With return (x > 0) ? false: true;
+
+
      **/
 }
