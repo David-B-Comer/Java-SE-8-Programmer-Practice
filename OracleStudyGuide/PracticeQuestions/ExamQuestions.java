38a61d8540181ec77005a40b8854ba57309d1837
David-B-Comer <davec2233@gmail.com>
2021-03-16 15:34:34 -0400

    added exam question 26

--- OracleStudyGuide/PracticeQuestions/ExamQuestions.java
+++ OracleStudyGuide/PracticeQuestions/ExamQuestions.java
@@ -844,5 +844,32 @@ public class ExamQuestions {
 
      ANSWER: A, C
 
+
+
+
+
+
+     Question #26
+     Given the code fragment:
+
+     public stat void main(String[] args) {
+        List<String> names = new ArrayList<>();
+        names.add("Robb");
+        names.add("Bran");
+        names.add("Rick");
+        names.add("Bran");
+
+        if (names.remove("Bran")) {
+            names.remove("Jon");
+        }
+        System.out.println(names);
+     }
+
+     What is the result?
+     A. [Robb, Rick, Bran]
+     B. [Robb, Rick]
+     C. [Robb, Bran, Rick, Bran]
+     D. An exception is thrown at runtime.
+
      **/
 }
