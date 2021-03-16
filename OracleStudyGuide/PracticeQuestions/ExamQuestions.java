18a5fa164e5422342b2866e5a77e878657088502
David-B-Comer <davec2233@gmail.com>
2021-03-15 21:11:48 -0400

    added exam question 23

--- OracleStudyGuide/PracticeQuestions/ExamQuestions.java
+++ OracleStudyGuide/PracticeQuestions/ExamQuestions.java
@@ -728,5 +728,26 @@ public class ExamQuestions {
 
      ANSWER: D
 
+
+
+
+
+     Question #23
+     Given the code fragment:
+
+     int x = 100;
+     int a = x++;
+     int b = ++x;
+     int c = x++;
+     int d = (a < b) ? (a < c) ? a: (b < c) ? b: c;
+     System.out.println(d);
+
+     What is the result?
+     A. 100
+     B. 101
+     C. 102
+     D. 103
+     E. Compilation fails
+
      **/
 }
