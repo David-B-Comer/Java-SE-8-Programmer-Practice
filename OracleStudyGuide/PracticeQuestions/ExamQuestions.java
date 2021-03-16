2b06ab67d428ada0d466a2bd5a47d1dab797c3e6
David-B-Comer <davec2233@gmail.com>
2021-03-16 15:40:33 -0400

    added exam question 27

--- OracleStudyGuide/PracticeQuestions/ExamQuestions.java
+++ OracleStudyGuide/PracticeQuestions/ExamQuestions.java
@@ -874,5 +874,38 @@ public class ExamQuestions {
 
      ANSWER: A
 
+
+
+
+
+
+     Question #27
+     Given:
+
+     class A {
+        public A() {
+            System.out.print("A ");
+        }
+     }
+     class B extends A{
+        public B() {                    // line n1
+            System.out.print("B ");
+        }
+     }
+     class extends B {
+        public C() {                    // line n2
+            System.out.print("C ");
+        }
+        public static void main(String[] args) {
+            C c = new C();
+        }
+     }
+
+     What is the result?
+     A. C B A
+     B. C
+     C. A B C
+     D. Compilation fails at line n1 and line n2
+
      **/
 }
