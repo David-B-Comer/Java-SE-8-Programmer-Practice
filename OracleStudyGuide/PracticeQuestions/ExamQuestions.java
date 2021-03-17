99807f4d34a1c40d318e93264e8bd1fc2d2ff759
David-B-Comer <davec2233@gmail.com>
2021-03-17 09:36:38 -0400

    added exam question 31

--- OracleStudyGuide/PracticeQuestions/ExamQuestions.java
+++ OracleStudyGuide/PracticeQuestions/ExamQuestions.java
@@ -999,5 +999,20 @@ public class ExamQuestions {
 
      ANSWER: C
 
+
+
+
+
+
+
+     Question #31
+     Which three are advantages of the Java exception mechanism?
+
+     A. Improves the program structure because the error handling code is separated from the normal program function
+     B. Provides a set of standard exceptions that covers all the possible errors
+     C. Improves the program structure because the programmer can choose where to handle exceptions
+     D. Improves the program structure because exceptions must be handled in the method in which they occurred
+     E. Allows the creation of new exceptions that are tailored to the particular program being created
+
      **/
 }
