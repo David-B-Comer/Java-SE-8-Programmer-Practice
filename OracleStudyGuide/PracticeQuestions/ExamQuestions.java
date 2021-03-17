3372c7a472985534e5a5aefde69716df66a6c379
David-B-Comer <davec2233@gmail.com>
2021-03-17 11:18:12 -0400

    added exam question 37

--- OracleStudyGuide/PracticeQuestions/ExamQuestions.java
+++ OracleStudyGuide/PracticeQuestions/ExamQuestions.java
@@ -1187,5 +1187,36 @@ public class ExamQuestions {
      ANSWER: A
 
 
+
+
+     Question #37
+     Given the code fragment:
+
+     public class Test {
+        static int count = 0;
+        int i = 0;
+
+        public void changeCount() {
+            while (i < 5) {
+                i++;
+                count++;
+            }
+        }
+
+        public static void main(String[] args) {
+            Test check1 = new Test();
+            Test check2 = new Test();
+            check1.changeCount();
+            check2.changeCount();
+            System.out.print(check1.count + " : " + check2.count);
+        }
+     }
+
+     What is the result?
+     A. 10 : 10
+     B. 5 : 5
+     C. 5 : 10
+     D. Compilation fails
+
      **/
 }
