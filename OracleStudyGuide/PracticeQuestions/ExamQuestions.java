2aab22407ef42e7ef73be7cd20714528b1764d0c
David-B-Comer <davec2233@gmail.com>
2021-03-17 10:53:27 -0400

    added exam question 34

--- OracleStudyGuide/PracticeQuestions/ExamQuestions.java
+++ OracleStudyGuide/PracticeQuestions/ExamQuestions.java
@@ -1107,5 +1107,26 @@ public class ExamQuestions {
 
      ANSWER: B
 
+
+
+
+
+     Question #34
+     Given the code fragment:
+
+     public static void main(String[] args) {
+        int ii = 0;
+        int jj = 7;
+        for (ii = 0; ii < jj -1; ii = ii + 2) {
+            System.out.print(ii + " ");
+        }
+     }
+
+     What is the result?
+     A. 2 4
+     B. 0 2 4 6
+     C. 0 2 4
+     D. Compilation fails
+
      **/
 }
