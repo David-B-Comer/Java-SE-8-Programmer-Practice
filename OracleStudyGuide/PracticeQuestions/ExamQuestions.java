06bb4fe5a0d193d12ee8e407984570953c751fbf
David-B-Comer <davec2233@gmail.com>
2021-03-17 11:10:59 -0400

    added exam question 36

--- OracleStudyGuide/PracticeQuestions/ExamQuestions.java
+++ OracleStudyGuide/PracticeQuestions/ExamQuestions.java
@@ -1164,5 +1164,24 @@ public class ExamQuestions {
 
      ANSWER: A
 
+
+
+
+
+     Question #36
+     Given the code fragment:
+
+     7. StringBuilder sb1 = new StringBuilder("Duke");
+     8. String str1 = sb1.toString();
+     9. // insert code here
+     10.    System.out.print(str1 == str2);
+
+
+     Which code fragment, when inserted at line 9, enables the code to print true?
+     A. String str2 = str1;
+     B. String str2 = new String (str1);
+     C. String str2 = sb1. toString ();
+     D. String str2 = "Duke";
+
      **/
 }
