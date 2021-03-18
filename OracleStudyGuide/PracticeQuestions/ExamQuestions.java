59df25cb14d298b1c640ca7c276a966412454160
David-B-Comer <davec2233@gmail.com>
2021-03-18 17:44:50 -0400

    added exam question 50

--- OracleStudyGuide/PracticeQuestions/ExamQuestions.java
+++ OracleStudyGuide/PracticeQuestions/ExamQuestions.java
@@ -1641,6 +1641,40 @@ public class ExamQuestions {
 
      ANSWER: C
 
+
+
+
+
+
+     Question #50
+     Given the code fragment:
+
+     public class App {
+        public static void main(String[] args) {
+            String str1 = "Java";
+            String str2 = new String("java");
+            // line n1
+            {
+                System.out.println("Equal");
+            } else {
+                System.out.println("Not Equal");
+            }
+        }
+     }
+
+     Which code fragment, when inserted at line n1, enables the App class to print Equal?
+
+     A) String str3 = str2;
+        if (str1 == str3)
+     B) if (str1.equalsIgnoreCase(str2))
+     C) String str3 = str2;
+        if (str1.equals(str3))
+     D) if (str1.toLowerCase() == str2.toLowerCase())
+
+     A. Option A
+     B. Option B
+     C. Option C
+     D. Option D
      **/
 }
 
