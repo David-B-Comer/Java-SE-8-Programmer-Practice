bfb00aaf7b2255efb8927116c38c0917dd0c275c
David-B-Comer <davec2233@gmail.com>
2021-03-18 14:50:50 -0400

    added exam question 39

--- OracleStudyGuide/PracticeQuestions/ExamQuestions.java
+++ OracleStudyGuide/PracticeQuestions/ExamQuestions.java
@@ -1265,5 +1265,34 @@ public class ExamQuestions {
 
      ANSWER: A, C
 
+
+
+
+     Question #39
+     Given:
+
+     public class Test {
+
+        public static void main(String[] args) {
+            if (args[0].equals("Hello") ? false : true) {
+                System.out.println("Success");
+            } else {
+                System.out.println("Failure");
+            }
+        }
+     }
+
+     And given the commands:
+     javac Test.Java
+
+     Java Test Hello -
+     What is the result?
+
+     A. Success
+     B. Failure
+     C. Compilation fails.
+     D. An exception is thrown at runtime
+
+
      **/
 }
