c9de80bcb2a7da4de2ed43799590002ec82ed054
David-B-Comer <davec2233@gmail.com>
2021-03-18 16:02:22 -0400

    added exam question 46

--- OracleStudyGuide/PracticeQuestions/ExamQuestions.java
+++ OracleStudyGuide/PracticeQuestions/ExamQuestions.java
@@ -1502,6 +1502,33 @@ public class ExamQuestions {
 
      ANSWER: C
 
+
+
+
+
+
+     Question #46
+     Given:
+
+     System.out.println("5 + 2 = " + 3 + 4);
+     System.out.println("5 + 2 = " + (3 + 4));
+
+     What is the result?
+
+     A) 5 + 2 = 34
+        5 + 2 = 34
+     B) 5 + 2 + 3 + 4
+        5 + 2 = 7
+     C) 7 = 7
+        7 + 7
+     D) 5 + 2 = 34
+        5 + 2 = 7
+
+     A. Option A
+     B. Option B
+     C. Option C
+     D. Option D
+
      **/
 }
 
