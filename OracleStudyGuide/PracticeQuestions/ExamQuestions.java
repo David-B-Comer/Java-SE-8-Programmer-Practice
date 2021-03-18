c5a60bddcfdb49de19bba37c2e277bea5068deaf
David-B-Comer <davec2233@gmail.com>
2021-03-18 15:48:02 -0400

    added exam question 45

--- OracleStudyGuide/PracticeQuestions/ExamQuestions.java
+++ OracleStudyGuide/PracticeQuestions/ExamQuestions.java
@@ -1467,6 +1467,36 @@ public class ExamQuestions {
 
      ANSWER: C
 
+
+
+
+
+
+     Question #45
+     Given the code fragment:
+
+     public static void main(String[] args) {
+        ArrayList myList = new ArrayList();
+        String[] myArray;
+        try {
+            while (true) {
+                myList.add("My String");
+            }
+        }
+        catch (Exception e) {
+            System.out.println("Caught an Exception");
+        }
+        System.out.println("Ready to use");
+     }
+
+     What is the result?
+     A. Execution terminates in the first catch statement, and caught a RuntimeException is printed to the console.
+     B. Execution terminates in the second catch statement, and caught an Exception is printed to the console.
+     C. A runtime error is thrown in the thread "main".
+     D. Execution completes normally, and Ready to use is printed to the console.
+     E. The code fails to compile because a throws keyword is required.
+
+
      **/
 }
 
