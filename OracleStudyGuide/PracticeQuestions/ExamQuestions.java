c9306fa683140854d812904ab5b11a51b123287e
David-B-Comer <davec2233@gmail.com>
2021-03-18 18:41:02 -0400

    added exam question 52

--- OracleStudyGuide/PracticeQuestions/ExamQuestions.java
+++ OracleStudyGuide/PracticeQuestions/ExamQuestions.java
@@ -1726,6 +1726,30 @@ public class ExamQuestions {
 
      ANSWER: B
 
+
+
+
+
+
+     Question #52
+     Given the code fragment:
+
+     String[] strs = new String[2];
+     int idx = 0;
+     for (String s : strs) {
+        strs[idx].concat(" element " + idx);
+        idx++;
+     }
+     for (idx = 0; idx < strs.length; idx++) {
+        System.out.println(strs[idx]);
+     }
+
+     What is the result?
+     A. Element 0 Element 1
+     B. Null element 0 Null element 1
+     C. Null Null
+     D. A NullPointerException is thrown at runtime.
+
      **/
 }
 
