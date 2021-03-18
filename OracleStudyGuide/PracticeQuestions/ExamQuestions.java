7c4b62812007e46664963128a392d1413c787c75
David-B-Comer <davec2233@gmail.com>
2021-03-18 18:33:27 -0400

    added exam question 51

--- OracleStudyGuide/PracticeQuestions/ExamQuestions.java
+++ OracleStudyGuide/PracticeQuestions/ExamQuestions.java
@@ -1678,6 +1678,51 @@ public class ExamQuestions {
 
 
      ANSWER: B
+
+
+
+
+
+     Question #51
+     Given:
+
+     public class sumTest {
+
+        public static void doSum(Integer x, Integer y) {
+            System.out.println("Integer sum is " + (x + y));
+        }
+        public static void doSum(double x, double y) {
+            System.out.println("double sum is " + (x + y));
+        }
+        public static void doSum(float x, float y) {
+            System.out.println("float sum is " + (x + y));
+        }
+        public static void doSum(int x, int y) {
+            System.out.println("int sum is " + (x + y));
+        }
+
+        public static void main(String[] args) {
+            doSum(10, 20):
+            doSum(10.0, 20.0);
+        }
+     }
+
+     What is the result?
+
+     A) int sum is 30
+        float sum is 30.0
+     B) int sum is 30
+        double sum is 30
+     C) Integer sum is 30
+        double sum is 30.0
+     D) Integer sum is 30
+        float sum is 30.0
+
+     A. Option A
+     B. Option B
+     C. Option C
+     D. Option D
+
      **/
 }
 
