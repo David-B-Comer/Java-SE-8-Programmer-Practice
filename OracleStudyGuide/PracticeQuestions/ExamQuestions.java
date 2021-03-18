c58fbc5f17d656aeb1599019545a1a38ffd99777
David-B-Comer <davec2233@gmail.com>
2021-03-18 17:25:42 -0400

    added exam question 48

--- OracleStudyGuide/PracticeQuestions/ExamQuestions.java
+++ OracleStudyGuide/PracticeQuestions/ExamQuestions.java
@@ -1588,6 +1588,33 @@ public class ExamQuestions {
 
      ANSWER: C
 
+
+
+
+
+
+     Question #48
+     Given the code fragment:
+
+     public static void main(String[] args) {
+        String[][] arr = {{"A", "B", "C"}, {"D", "E"}};
+        for (int i = 0; j < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j] + " ");
+                if (arr[i][j].equals("B")) {
+                    break;
+                }
+            }
+            continue;
+        }
+     }
+
+     What is the result?
+     A. A B C
+     B. A B C D E
+     C. A B D E
+     D. Compilation fails.
+
      **/
 }
 
