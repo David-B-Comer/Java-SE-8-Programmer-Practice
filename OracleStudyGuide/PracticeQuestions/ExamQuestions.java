25c17b70573f0af776fea95a0b54cdae29a98cde
David-B-Comer <davec2233@gmail.com>
2021-03-18 20:14:56 -0400

    added exam question 56

--- OracleStudyGuide/PracticeQuestions/ExamQuestions.java
+++ OracleStudyGuide/PracticeQuestions/ExamQuestions.java
@@ -1876,6 +1876,41 @@ public class ExamQuestions {
 
      ANSWER: A, D, F
 
+
+
+
+
+     Question #56
+     Given:
+
+     MainTest.java:
+
+     public class MainTest {
+        public static void main(int[] args) {
+            System.out.println("int main " + args[0]);
+        }
+        public static void main(Object[] args {
+            System.out.println("Object main " + args[0]);
+        }
+        public static void main(String[] args) {
+            System.out.println("String main " + args[0]);
+        }
+     }
+
+     and commands:
+
+     javac MainTest.java
+     java MainTest123
+
+
+     What is the result?
+
+     A. int main 1
+     B. Object main 1
+     C. String main 1
+     D. Compilation fails
+     E. An exception is thrown at runtime
+
      **/
 }
 
