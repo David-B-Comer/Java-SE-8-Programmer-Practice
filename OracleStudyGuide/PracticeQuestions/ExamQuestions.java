454a58a4077a3e6a9989ba25cecc64bf2f9ee078
David-B-Comer <davec2233@gmail.com>
2021-03-19 10:42:05 -0400

    added exam question 67

--- OracleStudyGuide/PracticeQuestions/ExamQuestions.java
+++ OracleStudyGuide/PracticeQuestions/ExamQuestions.java
@@ -2297,6 +2297,20 @@ public class ExamQuestions {
 
      ANSWER: D
 
+
+
+
+
+
+     Question #67
+     Which statement is true about Java byte code?
+
+     A. It can run on any platform.
+     B. It can run on any platform only if it was compiled for that platform.
+     C. It can run on any platform that has the Java Runtime Environment.
+     D. It can run on any platform that has a Java compiler.
+     E. It can run on any platform only if that platform has both the Java Runtime Environment and a Java compiler.
+
      **/
 }
 
