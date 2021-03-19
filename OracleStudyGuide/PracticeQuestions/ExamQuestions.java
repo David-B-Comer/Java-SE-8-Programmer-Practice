e4dc882c3d74dcf5a5b4b7e8a6a0078d8da173a8
David-B-Comer <davec2233@gmail.com>
2021-03-18 20:30:53 -0400

    added exam question 58

--- OracleStudyGuide/PracticeQuestions/ExamQuestions.java
+++ OracleStudyGuide/PracticeQuestions/ExamQuestions.java
@@ -1960,6 +1960,45 @@ public class ExamQuestions {
 
      ANSWER: A
 
+
+
+
+
+     Question #58
+     Given the code fragment:
+
+     public class Person {
+        String name;
+        int age = 25;
+
+        public Person(String name) {
+            this();             // line n1
+            setName(name);
+        }
+        public Person(String name, int age) {
+            Person(name);       // line n2
+            setAge(age);
+        }
+
+        // setter and getter methods go here
+        public String show() {
+            return name + " " + age + " " + number ;
+        }
+        public static void main(String[] args) {
+            Person p1 = new Person("Jesse");
+            Person p2 = new Person("Walter", 52);
+            System.out.println(p1.show());
+            System.out.println(p2.show());
+        }
+     }
+
+     What is the result?
+     A. Jesse 25 Walter 52
+     B. Compilation fails only at line n1
+     C. Compilation fails only at line n2
+     D. Compilation fails at both line n1 and line n2
+
+
      **/
 }
 
