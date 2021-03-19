9190014eeafcb4a92fa16d315ac209b3b34338c0
David-B-Comer <davec2233@gmail.com>
2021-03-18 20:44:44 -0400

    added exam question 59

--- OracleStudyGuide/PracticeQuestions/ExamQuestions.java
+++ OracleStudyGuide/PracticeQuestions/ExamQuestions.java
@@ -2001,6 +2001,62 @@ public class ExamQuestions {
 
      ANSWER: D
 
+
+
+
+
+
+
+     Question #59
+     Given the following code for a Planet object:
+
+     public class Planet {
+        public String name;
+        public int moons;
+
+        public Planet(String name, int moons) {
+            this.name = name;
+            this.moons = moons;
+        }
+     }
+     And the following main method:
+
+     public static void main(String[] args) {
+        Planet[] planets = {
+            new Planet("Mercury", 0),
+            new Planet("Venus", 0),
+            new Planet("Earth", 1),
+            new Planet("Mars", 3)
+        };
+        System.out.println(planets);
+        System.out.println(planets[2]);
+        System.out.println(planets[2].moons);
+     }
+
+     What is the output?
+
+     A) planets
+        Earth
+        1
+     B) [LPlanets.Planet;@15db9742
+        Earth
+        1
+     C) [LPlanets.Planet;@15db9742
+        Planets.Planet@6d06d69c
+        1
+     D) [LPlanets.Planet;@15db9742
+        Planets.Planet@6d06d69c
+        [LPlanets.Moon;@7852e922
+     E) [LPlanets.Planet;@15db9742
+        Venus
+        0
+
+     A. Option A
+     B. Option B
+     C. Option C
+     D. Option D
+     E. Option E
+
      **/
 }
 
