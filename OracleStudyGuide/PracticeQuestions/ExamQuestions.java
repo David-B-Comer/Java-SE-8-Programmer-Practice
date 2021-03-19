afebe3103e95c02a496b31587d7b75d7fa390c9b
David-B-Comer <davec2233@gmail.com>
2021-03-19 10:20:16 -0400

    added exam question 63

--- OracleStudyGuide/PracticeQuestions/ExamQuestions.java
+++ OracleStudyGuide/PracticeQuestions/ExamQuestions.java
@@ -2163,6 +2163,48 @@ public class ExamQuestions {
 
      ANSWER: B, E
 
+
+
+
+
+
+     Question #63
+     Given the content of three files:
+
+     A.java:
+
+     public class A {
+        public void a() { }
+        int a;
+     }
+
+     B.java:
+
+     public class B {
+        private int doStuff() {
+            private int x = 100;
+            return x++;
+        }
+     }
+
+     C.java:
+
+     import java.io.*;
+     package p1;
+     class A {
+        public void main(String fileName) throws IOException { }
+     }
+
+
+     Which statement is true?
+
+     A. Only the A.Java file compiles successfully.
+     B. Only the B.java file compiles successfully.
+     C. Only the C.java file compiles successfully.
+     D. The A.Java and B.java files compile successfully.
+     E. The B.java and C.java files compile successfully.
+     F. The A.Java and C.java files compile successfully.
+
      **/
 }
 
