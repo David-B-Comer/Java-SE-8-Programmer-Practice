b34eb07ec57d09b8fc6420a862e441934f155ad0
David-B-Comer <davec2233@gmail.com>
2021-03-20 20:32:47 -0400

    added exam question 73

--- OracleStudyGuide/PracticeQuestions/ExamQuestions.java
+++ OracleStudyGuide/PracticeQuestions/ExamQuestions.java
@@ -2474,6 +2474,44 @@ public class ExamQuestions {
 
      ANSWER: B
 
+
+
+
+
+
+
+     Question #73
+     Given:
+
+     public class App {
+
+        String myStr = "7007";
+
+        public void doStuff(String str) {
+            int myNum = 0;
+            try {
+                String myStr = str;
+                myNum = Integer.parseInt(myStr);
+            } catch (NumberFormatException ne) {
+                System.err.println("Error");
+            }
+            System.out.println(
+                "myStr: " + myStr + ", myNum: " + myNum);
+        }
+
+        public static void main(String[] args) {
+            App obj = new App();
+            obj.doStuff("9009");
+        }
+     }
+
+     What is the result?
+
+     A. myStr: 9009, myNum: 9009
+     B. myStr: 7007, myNum: 7007
+     C. myStr: 7007, myNum: 9009
+     D. Compilation fails
+
      **/
 }
 
