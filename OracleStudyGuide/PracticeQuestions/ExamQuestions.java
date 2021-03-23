f104cf18dbf7f89fa8efc6cb3d39eb721b1a8af5
David-B-Comer <davec2233@gmail.com>
2021-03-22 21:17:05 -0400

    added exam question 80

--- OracleStudyGuide/PracticeQuestions/ExamQuestions.java
+++ OracleStudyGuide/PracticeQuestions/ExamQuestions.java
@@ -2703,6 +2703,40 @@ public class ExamQuestions {
 
      ANSWER: C
 
+
+
+
+
+
+
+     Question #80
+     Given:
+
+     class Caller {
+        private void init() {
+            System.out.println("Initialized");
+        }
+
+        private void start() {
+        init();
+        System.out.println("Started");
+        }
+     }
+
+     public class TestCall {
+        public static void main(String[] args) {
+            Caller c - new Caller();
+            c.start();
+            c.init();
+        }
+     }
+
+     What is the result?
+     A. An exception is thrown at runtime.
+     B. Initialized Started Initialized
+     C. Initialized Started
+     D. Compilation fails.
+
      **/
 }
 
