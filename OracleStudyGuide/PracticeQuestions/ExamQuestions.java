c26642c63abbdfb26b09b784fef64095b3e8ab5b
David-B-Comer <davec2233@gmail.com>
2021-03-23 21:23:24 -0400

    added exam question 81

--- OracleStudyGuide/PracticeQuestions/ExamQuestions.java
+++ OracleStudyGuide/PracticeQuestions/ExamQuestions.java
@@ -2740,6 +2740,34 @@ public class ExamQuestions {
 
      ANSWER: D
 
+
+
+
+
+
+     Question #81
+     Given the code fragment:
+
+     public static void main(String[] args) {
+        try {
+            int num = 10;
+            int div = 0;
+            int ans = num / div;
+        } catch (ArithmeticException ae) {
+            ans = 0                                 // line n1
+        } catch (Exception e {
+            System.out.println("Invalid calculation");
+        }
+        System.out.println("Answer = " + ans);      // line n2
+     }
+
+     What is the result?
+     A. Answer = 0
+     B. Invalid calculation
+     C. Compilation fails only at line n1.
+     D. Compilation fails only at line n2.
+     E. Compilation fails only at line n1 and line2.
+
      **/
 }
 
