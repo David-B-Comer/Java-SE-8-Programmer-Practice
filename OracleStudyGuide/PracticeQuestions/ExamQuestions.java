c30c882aa011c16972c56fdbbf91378201aafef5
David-B-Comer <davec2233@gmail.com>
2021-03-24 17:40:00 -0400

    added exam question 86

--- OracleStudyGuide/PracticeQuestions/ExamQuestions.java
+++ OracleStudyGuide/PracticeQuestions/ExamQuestions.java
@@ -2893,6 +2893,32 @@ public class ExamQuestions {
 
      ANSWER: D
 
+
+
+
+
+     Question #86
+     Given:
+
+     public class Test {
+        public static void main(String[] args) {
+            boolean a = new Boolean(Boolean.valueOf(args[0]));
+            boolean b = new Boolean(args[1]);
+            System.out.println(a + " " + b);
+        }
+     }
+
+     And given the commands:
+     javac Test.java
+     java Test TRUE null
+
+     What is the result?
+     A. TRUE null
+     B. true false
+     C. false false
+     D. true true
+     E. A ClassCastException is thrown at runtime.
+
      **/
 }
 
