1985472b630fca4a76ab436236ece66eb7dc07cb
David-B-Comer <davec2233@gmail.com>
2021-03-25 20:18:02 -0400

    added exam question 88

--- OracleStudyGuide/PracticeQuestions/ExamQuestions.java
+++ OracleStudyGuide/PracticeQuestions/ExamQuestions.java
@@ -2966,6 +2966,18 @@ public class ExamQuestions {
 
      ANSWER: A
 
+
+
+
+
+     Question #88
+     Which statement will empty the contents of a StringBuilder variable named sb?
+
+     A. sb. deleteAll ();
+     B. sb. delete (0, sb. size () );
+     C. sb. delete (0, sb. length () );
+     D. sb. removeAll ();
+
      **/
 }
 
