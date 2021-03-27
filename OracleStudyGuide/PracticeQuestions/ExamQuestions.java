2b36120d0f3d48c075f82625a97f6d3160678286
David-B-Comer <davec2233@gmail.com>
2021-03-27 11:06:10 -0400

    added exam question 92

--- OracleStudyGuide/PracticeQuestions/ExamQuestions.java
+++ OracleStudyGuide/PracticeQuestions/ExamQuestions.java
@@ -3068,6 +3068,61 @@ public class ExamQuestions {
 
      ANSWER: D
 
+
+
+
+
+
+
+     Question #92
+     Given:
+
+     class Animal{
+        String type = "Canine";
+        int maxSpeed = 60;
+
+        Animal() {}
+
+        Animal(String type, int maxSpeed) {
+            this.type = type;
+            this.maxSpeed = maxSpeed;
+        }
+     }
+
+     class WildAnimal extends Animal{
+        String bounds;
+
+        WildAnimal(String bounds) {
+            // line n1
+        }
+
+        WildAnimal(String type, int maxSpeed, String bounds) {
+            // line n2
+        }
+     }
+
+
+     And given the code fragment:
+
+     7. WildAnimal wolf = new WildAnimal("Long");
+     8. WildAnimal tiger = new WildAnimal("Feline", 80, "Short");
+     9. System.out.println(wolf.type + " " + wolf.maxSpeed + " " + wolf.bounds);
+     10. System.out.println(tiger.type + " " + tiger.maxSpeed + " " + tiger.bounds);
+
+
+
+     Which two modifications enable the code to print the following output?
+
+     Canine 60 Long -
+
+     Feline 80 Short -
+
+     A. Replace line n1 with: super (); this.bounds = bounds;
+     B. Replace line n1 with: this.bounds = bounds; super ();
+     C. Replace line n2 with: super (type, maxSpeed); this (bounds);
+     D. Replace line n1 with: this ("Canine", 60); this.bounds = bounds
+     E. Replace line n2 with: super (type, maxSpeed); this.bounds = bounds;
+
      **/
 }
 
