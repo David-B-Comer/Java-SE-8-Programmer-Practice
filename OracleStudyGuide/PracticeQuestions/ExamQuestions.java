7896be69d9193f616b2334f8bb5136f4d18f3ebf
David-B-Comer <davec2233@gmail.com>
2021-03-28 12:48:39 -0400

    added exam question 95

--- OracleStudyGuide/PracticeQuestions/ExamQuestions.java
+++ OracleStudyGuide/PracticeQuestions/ExamQuestions.java
@@ -3215,6 +3215,46 @@ public class ExamQuestions {
 
      ANSWER: E
 
+
+
+
+
+     Question #95
+     Given the code fragments:
+
+     A.java:
+
+     package p1;
+     public class A  {
+     }
+
+     B.java:
+
+     package p1.p2;
+     // line n1
+     public class B {
+            public void doStuff() {
+                A b = new A();
+            }
+     }
+
+     C.java:
+
+     package p3;
+     // line n2
+     public class C {
+        public static void main(String[] args) {
+            A 01 = new A();
+            B 02 = new B();
+        }
+     }
+
+     Which modification enables the code to compile?
+     A. Replace line n1 with: import p1.*; Replace line n2 with: import p1. p2.*;
+     B. Replace line n1 with: import p1. A; Replace line n2 with: import p1.*;
+     C. Replace line n1 with: import p1. A; Replace line n2 with: import p1. A; import p1. p2.B ;
+     D. Replace line n1 with: import p1; Replace line n2 with: import p1;
+
      **/
 }
 
