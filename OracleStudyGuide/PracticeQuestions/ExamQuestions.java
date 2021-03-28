cd7b207c16a01d8f23ca56cf76e6b028b5b1f182
David-B-Comer <davec2233@gmail.com>
2021-03-28 12:41:56 -0400

    added exam question 94

--- OracleStudyGuide/PracticeQuestions/ExamQuestions.java
+++ OracleStudyGuide/PracticeQuestions/ExamQuestions.java
@@ -3161,6 +3161,57 @@ public class ExamQuestions {
 
      ANSWER: C
 
+
+
+
+
+
+     Question #94
+     Given the code fragment:
+
+     class Employee {
+        private String name;
+        private int age;
+        private int salary;
+
+        public Employee (String name, int age) {
+            setName(name)
+            setAge(age)
+            setSalary(2000);
+        }
+        public Employee (String name, int age, int salary) {
+            setSalary (salary);
+            this (name, age);
+        }
+        // getter and setter methods for attributes go here
+
+        public void printDetails() {
+            System.out.println (name + " : " + age + " : " + salary);
+        }
+     }
+
+
+     Test.java -
+
+     class Test {
+        public static void main (String[] args {
+            Employee e1 = new Employee();
+            Employee e2 = new Employee("Jack", 50);
+            Employee e3 = new Employee("Chloe", 40, 5000);
+            e1.printDetails();
+            e2.printDetails();
+            e3.printDetails();
+        }
+     }
+
+     Which is the result?
+
+     A. Compilation fails in the Employee class.
+     B. null : 0: 0 Jack : 50 : 0 Chloe : 40 : 5000
+     C. null : 0 : 0 Jack : 50 : 2000 Chloe : 40 : 5000
+     D. Compilation fails in the Test class.
+     E. Both the Employee class and the test class fail to compile.
+
      **/
 }
 
