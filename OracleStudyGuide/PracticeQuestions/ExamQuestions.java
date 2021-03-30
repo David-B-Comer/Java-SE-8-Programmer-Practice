15c307353d435c043b3f2026bb5317c3a0d8fec3
David-B-Comer <davec2233@gmail.com>
2021-03-30 19:40:55 -0400

    added exam question 99

--- OracleStudyGuide/PracticeQuestions/ExamQuestions.java
+++ OracleStudyGuide/PracticeQuestions/ExamQuestions.java
@@ -3384,6 +3384,34 @@ public class ExamQuestions {
 
     ANSWER: B
 
+
+
+
+
+
+     Question #99
+     You are asked to create a method that accepts an array of integers and returns the highest value from that array.
+     Given the code fragment:
+
+     class Test{
+        public static void main(String[] args) {
+            int numbers[] = {12, 13, 42, 32, 15, 156, 23, 51, 12};
+            int max = findMax(numbers);
+        }
+        // line n1 {
+        int max = 0;
+        // code goes here
+        return max;
+        }
+     }
+
+     Which method signature do you use at line n1?
+
+     A. public int findMax (int [] numbers)
+     B. static int[] findMax (int max)
+     C. static int findMax (int [] numbers)
+     D. final int findMax (int [] )
+
      **/
 }
 
